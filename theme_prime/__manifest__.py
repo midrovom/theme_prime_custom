3e85df2a3cbcfa52a473a61beec30cf810601b0e
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-10-28 16:34:59 -0500

    fix-update: ...

--- theme_prime/__manifest__.py
+++ theme_prime/__manifest__.py
@@ -125,7 +125,7 @@
             'theme_prime/static/src/xml/website_sale.xml',
 
             # Templates OWL Conedera Advance
-            'theme_prime/static/src/xml/customs/templates_custom_owl.xml',
+            'theme_prime/static/src/xml/customs/template_custom.xml',
         ],
         'web._assets_primary_variables': [
             'theme_prime/static/src/scss/primary_variables.scss',
