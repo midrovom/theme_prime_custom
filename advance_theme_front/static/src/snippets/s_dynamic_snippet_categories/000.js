2adb415660436563af19b05a7dbb9ccf3abf700a
midrovom <midrovo@callphoneecuador.com>
2025-11-10 14:45:06 -0500

    fix-update: aplicando cambios

--- advance_theme_front/static/src/snippets/s_dynamic_snippet_categories/000.js
+++ advance_theme_front/static/src/snippets/s_dynamic_snippet_categories/000.js
@@ -4,6 +4,20 @@ import DynamicSnippetCarousel from "@website/snippets/s_dynamic_snippet_carousel
 
 const DynamicSnippetCategories = DynamicSnippetCarousel.extend({
     selector: ".s_dynamic_snippet_categories",
+
+    _getSearchDomain() {
+        // Aquí puedes devolver [] o algún filtro por categoría si quieres
+        return [];
+    },
+
+    _getRpcParameters() {
+        return {}; // No necesitas parámetros especiales de momento
+    },
+
+    _getMainPageUrl() {
+        return "/shop";
+    },
+    
 });
 
 publicWidget.registry.dynamic_snippet_categories = DynamicSnippetCategories;
