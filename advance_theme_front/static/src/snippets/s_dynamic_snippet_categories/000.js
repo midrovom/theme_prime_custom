b878791496d48c1b87cf93fb0c5d5a01a22a8237
midrovom <midrovo@callphoneecuador.com>
2025-11-10 16:29:40 -0500

    fix-update: aplicando cambios

--- advance_theme_front/static/src/snippets/s_dynamic_snippet_categories/000.js
+++ advance_theme_front/static/src/snippets/s_dynamic_snippet_categories/000.js
@@ -5,14 +5,119 @@ import DynamicSnippetCarousel from "@website/snippets/s_dynamic_snippet_carousel
 const DynamicSnippetCategories = DynamicSnippetCarousel.extend({
     selector: ".s_dynamic_snippet_categories",
 
-    init() {
-        this._super.apply(this, arguments);
-        this.isAlternativeProductSnippet = false; // asegura que filter_opt se muestre
-    },
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * Gets the category search domain
+     *
+     * @private
+     */
     _getCategorySearchDomain() {
-        // aquí defines cómo tu snippet de categorías filtra registros
-        return [];
-    }
+        const searchDomain = [];
+        let productCategoryId = this.$el.get(0).dataset.productCategoryId;
+        if (productCategoryId && productCategoryId !== 'all') {
+            if (productCategoryId === 'current') {
+                productCategoryId = undefined;
+                const productCategoryField = $("#product_details").find(".product_category_id");
+                if (productCategoryField && productCategoryField.length) {
+                    productCategoryId = parseInt(productCategoryField[0].value);
+                }
+                if (!productCategoryId) {
+                    this.trigger_up('main_object_request', {
+                        callback: function (value) {
+                            if (value.model === "product.public.category") {
+                                productCategoryId = value.id;
+                            }
+                        },
+                    });
+                }
+                if (!productCategoryId) {
+                    // Try with categories from product, unfortunately the category hierarchy is not matched with this approach
+                    const productTemplateId = $("#product_details").find(".product_template_id");
+                    if (productTemplateId && productTemplateId.length) {
+                        searchDomain.push(['public_categ_ids.product_tmpl_ids', '=', parseInt(productTemplateId[0].value)]);
+                    }
+                }
+            }
+            if (productCategoryId) {
+                searchDomain.push(['public_categ_ids', 'child_of', parseInt(productCategoryId)]);
+            }
+        }
+        return searchDomain;
+    },
+    /**
+     * Gets the tag search domain
+     *
+     * @private
+     */
+    _getTagSearchDomain() {
+        const searchDomain = [];
+        let productTagIds = this.$el.get(0).dataset.productTagIds;
+        productTagIds = productTagIds ? JSON.parse(productTagIds) : [];
+        if (productTagIds.length) {
+            searchDomain.push(['all_product_tag_ids', 'in', productTagIds.map(productTag => productTag.id)]);
+        }
+        return searchDomain;
+    },
+    /**
+     * Method to be overridden in child components in order to provide a search
+     * domain if needed.
+     * @override
+     * @private
+     */
+    _getSearchDomain: function () {
+        const searchDomain = this._super.apply(this, arguments);
+        searchDomain.push(...this._getCategorySearchDomain());
+        searchDomain.push(...this._getTagSearchDomain());
+        const productNames = this.$el.get(0).dataset.productNames;
+        if (productNames) {
+            const nameDomain = [];
+            for (const productName of productNames.split(',')) {
+                // Ignore empty names
+                if (!productName.length) {
+                    continue;
+                }
+                // Search on name, internal reference and barcode.
+                if (nameDomain.length) {
+                    nameDomain.unshift('|');
+                }
+                nameDomain.push(...[
+                    '|', '|', ['name', 'ilike', productName],
+                              ['default_code', '=', productName],
+                              ['barcode', '=', productName],
+                ]);
+            }
+            searchDomain.push(...nameDomain);
+        }
+        if (!this.el.dataset.showVariants) {
+            searchDomain.push('hide_variants')
+        }
+        return searchDomain;
+    },
+    /**
+     * Add `productTemplateId` for product snippets (Accessories, Alternatives and Recently sold).
+     *
+     * See `dynamic_snippet_accessories_action`, `dynamic_snippet_recently_sold_with_action` and
+     * `dynamic_snippet_alternative_products`.
+     *
+     * @override
+     * @private
+     */
+    _getRpcParameters: function () {
+        const productTemplateId = $("#product_details").find(".product_template_id");
+        return Object.assign(this._super.apply(this, arguments), {
+            productTemplateId: productTemplateId && productTemplateId.length ? productTemplateId[0].value : undefined,
+        });
+    },
+    /**
+     * @override
+     * @private
+     */
+    _getMainPageUrl() {
+        return "/shop";
+    },
 
 
 });
