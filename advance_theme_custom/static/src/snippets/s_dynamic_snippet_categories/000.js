c92201c1a4b76a72f95f3f1383fe2446972b315a
midrovom <midrovo@callphoneecuador.com>
2025-11-10 12:04:00 -0500

    fix-update: aplicando cambios

--- advance_theme_custom/static/src/snippets/s_dynamic_snippet_categories/000.js
+++
@@ -1,9 +0,0 @@
-/** @odoo-module **/
-import publicWidget from "@web/legacy/js/public/public_widget";
-import DynamicSnippetCarousel from "@website/snippets/s_dynamic_snippet_carousel/000";
-
-const DynamicSnippetCategories = DynamicSnippetCarousel.extend({
-    selector: ".s_dynamic_snippet_categories",
-});
-
-publicWidget.registry.dynamic_snippet_categories = DynamicSnippetCategories;
