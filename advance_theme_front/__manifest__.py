9b44a290b417fd2964c67bd0b67b7cbd1f9a5b3c
midrovom <midrovo@callphoneecuador.com>
2025-11-10 12:04:08 -0500

    fix-update: aplicando cambios

---
+++ advance_theme_front/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'Advance Theme Front',
+    'version': '18.0.1.0.0',
+    'description': 'Advance Theme Front',
+    'summary': 'Advance Theme Front',
+    'author': 'Mauricio Idrovo',
+    'website': 'https://callphoneecuador.com',
+    'license': 'LGPL-3',
+    'category': 'Website/Website',
+    'depends': [
+        'website',
+        'website_sale',
+    ],
+    'data': [
+        'data/category_snippet_template_data',
+        'views/snippets/s_dynamic_snippet_categories_preview_data.xml',
+        'views/snippets/s_dynamic_snippet_category.xml',
+        'views/snippets/snippets.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'assets': {
+
+    }
+}
---
+++ advance_theme_front/static/src/snippets/s_dynamic_snippet_categories/000.js
@@ -0,0 +1,9 @@
+/** @odoo-module **/
+import publicWidget from "@web/legacy/js/public/public_widget";
+import DynamicSnippetCarousel from "@website/snippets/s_dynamic_snippet_carousel/000";
+
+const DynamicSnippetCategories = DynamicSnippetCarousel.extend({
+    selector: ".s_dynamic_snippet_categories",
+});
+
+publicWidget.registry.dynamic_snippet_categories = DynamicSnippetCategories;
