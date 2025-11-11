d2bcc2e7fc12110fd3f0e890f6924b3c13dda62c
midrovom <midrovo@callphoneecuador.com>
2025-11-11 15:35:03 -0500

    fix-update: aplicando cambios

--- advance_theme_front/controllers/main.py
+++ advance_theme_front/controllers/main.py
@@ -1,4 +1,4 @@
-from odoo.addons.portal.controllers.web import Home
+from odoo.addons.website.controllers.main import Website
 from odoo import http, models, fields, _
 
 import logging
@@ -6,7 +6,7 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
-class WebsiteCustom(Home):
+class WebsiteCustom(Website):
 
     @http.route('/website/snippet/filters', type='json', auth='public', website=True, readonly=True)
     def get_dynamic_filter(self, filter_id, template_key, limit=None, search_domain=None, with_sample=False, **custom_template_data):
@@ -27,4 +27,5 @@ class WebsiteCustom(Home):
         _logger.info(f"MOSTRANDO RES ************************************** { res }")
 
         return res
+    
     
