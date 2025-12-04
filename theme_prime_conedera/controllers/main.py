c2d27eaa66247dca41e30a820ac29369634d8c16
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-04 11:31:40 -0500

    fix-update: ...

--- theme_prime_conedera/controllers/main.py
+++ theme_prime_conedera/controllers/main.py
@@ -1,6 +1,8 @@
+# from odoo.addons.theme_prime.controllers.main import ThemePrimeMainClass
+# from odoo.http import request
+# from odoo.tools import formatLang
+
 from odoo.addons.theme_prime.controllers.main import ThemePrimeMainClass
-from odoo.http import request
-from odoo.tools import formatLang
 
 class ThemePrimeMainClassExtended(ThemePrimeMainClass):
 
@@ -10,23 +12,43 @@ class ThemePrimeMainClassExtended(ThemePrimeMainClass):
         result = super()._prepare_product_data(products, fields, pricelist, options)
 
         for res_product, product in zip(result, products):
-            res_product['attributes'] = []
-            res_product['other_attributes'] = []
+            # Inicializamos la lista de marcas
+            res_product['brands'] = []
 
             for line in product.attribute_line_ids:
+                # Solo tomamos los atributos que tengan el check dr_is_brand
                 if line.attribute_id.dr_is_brand:
-                    continue
-
-                for val in line.value_ids:
-                    attr_data = {
-                        'id': val.id,
-                        'name': val.name,
-                        'image': val.dr_image and f'/web/image/product.attribute.value/{val.id}/dr_image' or False,
-                        'attribute_name': line.attribute_id.name,
-                    }
-                    if line.attribute_id.attribute_custom:
-                        res_product['attributes'].append(attr_data)
-                    else:
-                        res_product['other_attributes'].append(attr_data)
+                    for val in line.value_ids:
+                        # Guardamos únicamente el campo name
+                        res_product['brands'].append(val.name)
 
         return result
+
+# class ThemePrimeMainClassExtended(ThemePrimeMainClass):
+
+#     def _prepare_product_data(self, products, fields, pricelist, options=None):
+#         fields = list(set(fields or []))
+#         fields += ['attribute_line_ids']
+#         result = super()._prepare_product_data(products, fields, pricelist, options)
+
+#         for res_product, product in zip(result, products):
+#             res_product['attributes'] = []
+#             res_product['other_attributes'] = []
+
+#             for line in product.attribute_line_ids:
+#                 if line.attribute_id.dr_is_brand:
+#                     continue
+
+#                 for val in line.value_ids:
+#                     attr_data = {
+#                         'id': val.id,
+#                         'name': val.name,
+#                         'image': val.dr_image and f'/web/image/product.attribute.value/{val.id}/dr_image' or False,
+#                         'attribute_name': line.attribute_id.name,
+#                     }
+#                     if line.attribute_id.attribute_custom:
+#                         res_product['attributes'].append(attr_data)
+#                     else:
+#                         res_product['other_attributes'].append(attr_data)
+
+#         return result
