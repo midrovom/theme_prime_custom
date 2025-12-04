87d716377932d4725197aae26b5fff01017746ec
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-04 16:43:23 -0500

    fix-update: ...

--- conedera_theme_common/controller/main.py
+++ conedera_theme_common/controller/main.py
@@ -1,23 +1,12 @@
 from odoo import http
 from odoo.http import request
 
-class WebsiteBrandProducts(http.Controller):
+class WebsiteSaleBrands(http.Controller):
 
-    @http.route('/website/snippet/brand/products', type='json', auth='public')
-    def brand_products(self, brand_id=None, limit=12):
-        Brand = request.env['product.brand'].sudo()
-
-        if not brand_id:
-            return []
-
-        products = request.env['product.template'].sudo().search([
-            ('brand_id', '=', int(brand_id)),
-            ('website_published', '=', True),
-        ], limit=limit)
-
-        return [{
-            "name": p.name,
-            "price": p.website_price,
-            "url": f"/shop/product/{p.id}",
-            "image": f"/web/image/product.template/{p.id}/image_1920",
-        } for p in products]
+    @http.route(['/website_sale/get_products_by_brand'], type='json', auth='public', website=True)
+    def get_products_by_brand(self, brand_id=None, limit=16):
+        snippet_filter = request.env['website.snippet.filter'].sudo()
+        return {
+            'records': snippet_filter._get_products_by_brand(brand_id, limit),
+            'is_sample': False,
+        }
--- conedera_theme_common/models/website_snippet_filter.py
+++ conedera_theme_common/models/website_snippet_filter.py
@@ -1,37 +1,18 @@
-from odoo import _, api, fields, models
-from odoo.http import request
+from odoo import models, api
+from odoo.osv import expression
 
 class WebsiteSnippetFilter(models.Model):
     _inherit = 'website.snippet.filter'
 
-    def _filter_records_to_values(self, records, is_sample=False):
-        res = super()._filter_records_to_values(records, is_sample)
-
-        if self.model_name == 'product.brand':
-            for data in res:
-                brand = data['_record']
-
-                data['url'] = "/shop?brand_id=%s" % brand.id
-
-                data['product_count'] = request.env['product.template'].sudo().search_count([
-                    ('brand_id', '=', brand.id),
-                    ('website_published', '=', True),
-                ])
-
-                if not data.get('image_512'):
-                    data['image_512'] = "/web/static/img/placeholder.png"
-
-        return res
-
     @api.model
-    def _get_public_brands(self, mode=None, **kwargs):
+    def _get_products_by_brand(self, brand_id, limit=None, **kwargs):
         website = self.env['website'].get_current_website()
-
         domain = [
+            ('website_published', '=', True),
+            ('sale_ok', '=', True),
             ('website_id', 'in', [False, website.id]),
-            ('active', '=', True),
         ]
-
-        brands = self.env['product.brand'].search(domain, order="sequence ASC, name ASC")
-
-        return self._filter_records_to_values(brands)
+        if brand_id:
+            domain.append(('product_tmpl_id.brand_id', '=', brand_id))
+        products = self.env['product.product'].search(domain, limit=limit or self.limit)
+        return self._filter_records_to_values(products, is_sample=False)
