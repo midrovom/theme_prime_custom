bec2b006e3c7f66d211c785c6f85b7e6d3dbaa6f
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-04 14:54:42 -0500

    fix-update: ...

--- conedera_theme_common/__manifest__.py
+++ conedera_theme_common/__manifest__.py
@@ -15,8 +15,14 @@
         'droggol_theme_common',
     ],
     'data': [
+
+        'data/data.xml',
+        'data/brands_snippet_template_data.xml',
+
         'views/website_template.xml',
 
+        'views/snippets/s_dynamic_snippet_brands_preview_data.xml',
+        'views/snippets/s_dynamic_snippet_category.xml',
         'views/snippets/snippets.xml',
         'views/snippets/s_key_images_custom.xml',
         
