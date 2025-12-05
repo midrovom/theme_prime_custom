823f28fe1ed0fc6cff8a228977fe1b9461d27329
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-05 15:10:06 -0500

    fix-update: ...

--- conedera_theme_common/static/src/snippets/s_dynamic_snippet_brands/option.js
+++ conedera_theme_common/static/src/snippets/s_dynamic_snippet_brands/option.js
@@ -2,18 +2,14 @@ import options from "@web_editor/js/editor/snippets.options";
 import s_dynamic_snippet_carousel_options from "@website/snippets/s_dynamic_snippet_carousel/options";
 
 const dynamicSnippetBrandOptions = s_dynamic_snippet_carousel_options.extend({
-    /**
-     * @override
-     */
 
-    // Override que define el filtro del modelo específico para marcas
     init() {
         this._super.apply(this, arguments);
+
         this.modelNameFilter = 'product.attribute.value';  
     },
 });
 
-// Registrar en el editor como nuevo snippet de marcas
-options.registry.dynamic_snippet_brands = dynamicSnippetBrandOptions;
+options.registry.dynamic_snippet_brand = dynamicSnippetBrandOptions;
 
 export default dynamicSnippetBrandOptions;
