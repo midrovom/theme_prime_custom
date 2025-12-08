5464c22f5da5f765c9aeeaf5ca75d387d15f3423
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-08 10:42:04 -0500

    fix-update: ...

---
+++ conedera_theme_common/models/website_snippet_filter.py
@@ -0,0 +1,18 @@
+from odoo import api, models
+from odoo.osv import expression
+
+class WebsiteSnippetFilter(models.Model):
+    _inherit = 'website.snippet.filter'
+
+    @api.model
+    def _get_products_by_brand(self, website, limit, domain, **kwargs):
+        """Devuelve productos filtrados por marca (dr_brand_value_id)."""
+        brand_id = kwargs.get('brand_id')
+        products = self.env['product.template']
+        if brand_id:
+            domain = expression.AND([
+                domain,
+                [('dr_brand_value_id', '=', int(brand_id))],
+            ])
+        products = products.with_context(display_default_code=False).search(domain, limit=limit)
+        return products
