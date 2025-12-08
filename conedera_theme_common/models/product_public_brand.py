fec3a0f1932f852956a0510cbbab8ca4cf9f41f2
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-08 09:54:35 -0500

    fix-update: ...

--- conedera_theme_common/models/product_public_brand.py
+++ conedera_theme_common/models/product_public_brand.py
@@ -1,10 +1,25 @@
-from odoo import models, fields
+from odoo import api, fields, models
 
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
+class ProductPublicBrand(models.Model):
+    _name = 'product.public.brand'
+    _description = "Website Product Brand"
+    _order = 'name, id'
 
-    dr_brand_value_id = fields.Many2one(
-        related="product_variant_ids.dr_brand_value_id",
-        store=True,
-        readonly=True,
+    name = fields.Char(required=True, translate=True)
+    sequence = fields.Integer(default=10, index=True)
+
+    product_ids = fields.One2many(
+        comodel_name='product.product',
+        inverse_name='dr_brand_value_id',
+        string="Products"
+    )
+
+    website_description = fields.Html(
+        string="Brand Description",
+        translate=True
+    )
+
+    website_footer = fields.Html(
+        string="Brand Footer",
+        translate=True
     )
