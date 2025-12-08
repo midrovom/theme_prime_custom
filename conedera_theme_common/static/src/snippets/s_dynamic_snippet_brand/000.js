4a79eb96faf2485d7bb1150e62fd1b3e5cc6f544
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-08 09:56:39 -0500

    fix-update: ...

--- conedera_theme_common/static/src/snippets/s_dynamic_snippet_brand/000.js
+++ conedera_theme_common/static/src/snippets/s_dynamic_snippet_brand/000.js
@@ -5,24 +5,36 @@ import DynamicSnippetProducts from "@website/snippets/s_dynamic_snippet_products
 
 const DynamicSnippetProductsBrand = DynamicSnippetProducts.extend({
 
+    /**
+     * Gets the brand search domain
+     *
+     * @private
+     */
     _getBrandSearchDomain() {
-        const domain = [];
-        const brandId = this.$el[0].dataset.productBrandId;
-
-        if (brandId && brandId !== "all") {
-            domain.push(['dr_brand_value_id', '=', parseInt(brandId)]);
+        const searchDomain = [];
+        let productBrandId = this.$el.get(0).dataset.productBrandId;
+        if (productBrandId && productBrandId !== 'all') {
+            // 🔹 Filtra directamente por el campo dr_brand_value_id en product.product
+            searchDomain.push(['dr_brand_value_id', '=', parseInt(productBrandId)]);
         }
-        return domain;
+        return searchDomain;
     },
 
-    searchDomainFromUI: function () {
-        const domain = this._super.apply(this, arguments);
-        domain.push(...this._getBrandSearchDomain());
-        return domain;
+    /**
+     * Override search domain to include brand
+     *
+     * @override
+     * @private
+     */
+    _getSearchDomain: function () {
+        const searchDomain = this._super.apply(this, arguments);
+        searchDomain.push(...this._getBrandSearchDomain());
+        return searchDomain;
     },
 });
 
 publicWidget.registry.dynamic_snippet_products_brand = DynamicSnippetProductsBrand;
+
 export default DynamicSnippetProductsBrand;
 
 // /** @odoo-module **/
--- conedera_theme_common/static/src/snippets/s_dynamic_snippet_brand/option.js
+++ conedera_theme_common/static/src/snippets/s_dynamic_snippet_brand/option.js
@@ -6,21 +6,25 @@ import wUtils from "@website/js/utils";
 
 const alternativeSnippetRemovedOptions = [
     'filter_opt', 'product_category_opt', 'product_tag_opt', 'product_names_opt',
-];
+]
 
 const dynamicSnippetProductsOptions = s_dynamic_snippet_carousel_options.extend({
 
-    init() {
+    init: function () {
         this._super.apply(this, arguments);
-
         this.modelNameFilter = 'product.product';
-        this.productBrands = {};
+
+        const productTemplateId = this.$target.closest("#wrapwrap").find("input.product_template_id");
+        this.hasProductTemplateId = productTemplateId.val();
+        if (!this.hasProductTemplateId) {
+            this.contextualFilterDomain.push(['product_cross_selling', '=', false]);
+        }
+
         this.productCategories = {};
+        this.productBrands = {};   // 🔹 nuevo diccionario para marcas
+        this.isAlternativeProductSnippet = this.$target.hasClass('o_wsale_alternative_products');
 
         this.orm = this.bindService("orm");
-
-        this.isAlternativeProductSnippet =
-            this.$target.hasClass("o_wsale_alternative_products");
     },
 
     _computeWidgetVisibility(widgetName, params) {
@@ -30,75 +34,79 @@ const dynamicSnippetProductsOptions = s_dynamic_snippet_carousel_options.extend(
         return this._super(...arguments);
     },
 
-    //----------------------------------------------------------------------
+    //--------------------------------------------------------------------------
     // Fetchers
-    //----------------------------------------------------------------------
+    //--------------------------------------------------------------------------
 
+    _fetchProductCategories: function () {
+        return this.orm.searchRead("product.public.category", wUtils.websiteDomain(this), ["id", "name"]);
+    },
+
+    /**
+     * 🔹 Nuevo: Fetches product brands desde product.attribute.value
+     */
     _fetchProductBrands: async function () {
-        const brandAttr = await this.orm.searchRead(
+        // Busca el atributo "Marca"
+        const brandAttribute = await this.orm.searchRead(
             "product.attribute",
-            [["name", "=", "Brand"]],
+            [["name", "=", "Marca"]],
             ["id"]
         );
-
-        if (!brandAttr.length) {
+        if (!brandAttribute.length) {
             return [];
         }
+        const brandAttributeId = brandAttribute[0].id;
 
+        // Trae los valores de ese atributo
         return this.orm.searchRead(
             "product.attribute.value",
-            [["attribute_id", "=", brandAttr[0].id]],
+            [["attribute_id", "=", brandAttributeId]],
             ["id", "name"]
         );
     },
 
-    _fetchProductCategories() {
-        return this.orm.searchRead(
-            "product.public.category",
-            wUtils.websiteDomain(this),
-            ["id", "name"]
-        );
-    },
-
-    //----------------------------------------------------------------------
-    // Render
-    //----------------------------------------------------------------------
+    //--------------------------------------------------------------------------
+    // Renderers
+    //--------------------------------------------------------------------------
 
     async _renderCustomXML(uiFragment) {
-        await this._super(...arguments);
+        await this._super.apply(this, arguments);
         await this._renderProductCategorySelector(uiFragment);
-        await this._renderProductBrandSelector(uiFragment);
+        await this._renderProductBrandSelector(uiFragment);   
     },
 
-    async _renderProductBrandSelector(uiFragment) {
-        const brands = await this._fetchProductBrands();
-        for (let b of brands) {
-            this.productBrands[b.id] = b;
+    _renderProductCategorySelector: async function (uiFragment) {
+        const productCategories = await this._fetchProductCategories();
+        for (let index in productCategories) {
+            this.productCategories[productCategories[index].id] = productCategories[index];
         }
-        const brandSelector = uiFragment.querySelector('[data-name="product_brand_opt"]');
-        return this._renderSelectUserValueWidgetButtons(brandSelector, this.productBrands);
+        const productCategoriesSelectorEl = uiFragment.querySelector('[data-name="product_category_opt"]');
+        return this._renderSelectUserValueWidgetButtons(productCategoriesSelectorEl, this.productCategories);
     },
 
-    async _renderProductCategorySelector(uiFragment) {
-        const categories = await this._fetchProductCategories();
-        for (let c of categories) {
-            this.productCategories[c.id] = c;
+    /**
+     * 🔹 Nuevo: Renderiza las marcas en el builder
+     */
+    _renderProductBrandSelector: async function (uiFragment) {
+        const productBrands = await this._fetchProductBrands();
+        for (let index in productBrands) {
+            this.productBrands[productBrands[index].id] = productBrands[index];
         }
-        const categorySelector = uiFragment.querySelector('[data-name="product_category_opt"]');
-        return this._renderSelectUserValueWidgetButtons(categorySelector, this.productCategories);
+        const productBrandsSelectorEl = uiFragment.querySelector('[data-name="product_brand_opt"]');
+        return this._renderSelectUserValueWidgetButtons(productBrandsSelectorEl, this.productBrands);
     },
 
-    _setOptionsDefaultValues() {
-        this._setOptionValue("productBrandId", "all");
-        this._setOptionValue("productCategoryId", "all");
-        this._setOptionValue("showVariants", true);
-        this._super(...arguments);
+    _setOptionsDefaultValues: function () {
+        this._setOptionValue('productCategoryId', 'all');
+        this._setOptionValue('productBrandId', 'all');   
+        this._setOptionValue('showVariants', true);
+        this._super.apply(this, arguments);
     },
 });
 
 options.registry.dynamic_snippet_products = dynamicSnippetProductsOptions;
-export default dynamicSnippetProductsOptions;
 
+export default dynamicSnippetProductsOptions;
 
 // /** @odoo-module **/
 
