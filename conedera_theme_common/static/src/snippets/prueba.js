eb39c3f8a59f4438827eb93409e97ac495408754
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-09 16:20:32 -0500

    fix-update: ...

--- conedera_theme_common/static/src/snippets/prueba.js
+++ conedera_theme_common/static/src/snippets/prueba.js
@@ -7,7 +7,7 @@ import DynamicSnippetProducts from "@website/snippets/s_dynamic_snippet_products
 const DynamicSnippetProductsBrand = DynamicSnippetProducts.extend({
 
     /**
-     * Nuevo método para filtrar por marca
+     * Nuevo método para filtrar por marca (domain opcional)
      */
     _getBrandSearchDomain() {
         const searchDomain = [];
@@ -26,9 +26,55 @@ const DynamicSnippetProductsBrand = DynamicSnippetProducts.extend({
         searchDomain.push(...this._getBrandSearchDomain());
         return searchDomain;
     },
+
+    /**
+     * NUEVO: Sobrescribimos el contexto para pasar product_brand_id
+     */
+    _getSearchContext: function () {
+        const searchContext = this._super.apply(this, arguments);
+        let productBrandId = this.$el.get(0).dataset.productBrandId;
+        if (productBrandId && productBrandId !== 'all') {
+            searchContext.product_brand_id = parseInt(productBrandId);
+        }
+        return searchContext;
+    },
 });
 
-// Registramos el nuevo widget en el mismo selector
 publicWidget.registry.dynamic_snippet_products = DynamicSnippetProductsBrand;
 
 export default DynamicSnippetProductsBrand;
+
+// /** @odoo-module **/
+
+// import publicWidget from "@web/legacy/js/public/public_widget";
+// import DynamicSnippetProducts from "@website/snippets/s_dynamic_snippet_products/000";  // importa el original
+
+// // Extendemos el snippet original
+// const DynamicSnippetProductsBrand = DynamicSnippetProducts.extend({
+
+//     /**
+//      * Nuevo método para filtrar por marca
+//      */
+//     _getBrandSearchDomain() {
+//         const searchDomain = [];
+//         let productBrandId = this.$el.get(0).dataset.productBrandId;
+//         if (productBrandId && productBrandId !== 'all') {
+//             searchDomain.push(['dr_brand_value_id', '=', parseInt(productBrandId)]);
+//         }
+//         return searchDomain;
+//     },
+
+//     /**
+//      * Sobrescribimos el searchDomain para incluir marcas
+//      */
+//     _getSearchDomain: function () {
+//         const searchDomain = this._super.apply(this, arguments);
+//         searchDomain.push(...this._getBrandSearchDomain());
+//         return searchDomain;
+//     },
+// });
+
+// // Registramos el nuevo widget en el mismo selector
+// publicWidget.registry.dynamic_snippet_products = DynamicSnippetProductsBrand;
+
+// export default DynamicSnippetProductsBrand;
