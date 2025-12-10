9f138a5d099549bda1c53e38d93037147483580d
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-10 15:04:53 -0500

    fix-update: ...

--- conedera_theme_common/static/src/snippets/s_dynamic_snippet_brand/option.js
+++ conedera_theme_common/static/src/snippets/s_dynamic_snippet_brand/option.js
@@ -14,11 +14,12 @@ const dynamicSnippetProductsOptionsBrand = dynamicSnippetProductsOptions.extend(
     // Fetch marcas
     // -------------------------------
     _fetchProductBrands: function () {
-        return this.orm.searchRead("product.attribute.value", [["attribute_id.dr_is_brand", "=", true]], ["id", "name", "attribute_id"]);
+        return this.orm.searchRead(
+            "product.attribute.value",
+            [["attribute_id.dr_is_brand", "=", true]],
+            ["id", "name", "attribute_id"]
+        );
     },
-    // _fetchProductBrands: function () {
-    //     return this.orm.searchRead("product.template.attribute.line", wUtils.websiteDomain(this), ["id", "name"]);
-    // },
 
     // -------------------------------
     // Render marcas
@@ -43,6 +44,55 @@ const dynamicSnippetProductsOptionsBrand = dynamicSnippetProductsOptions.extend(
     },
 });
 
-options.registry.dynamic_snippet_products = dynamicSnippetProductsOptionsBrand;
+options.registry.dynamic_snippet_products_brand = dynamicSnippetProductsOptionsBrand;
 
 export default dynamicSnippetProductsOptionsBrand;
+
+// /** @odoo-module **/
+
+// import options from "@web_editor/js/editor/snippets.options";
+// import dynamicSnippetProductsOptions from "@website/snippets/s_dynamic_snippet_carousel/options";  
+
+// const dynamicSnippetProductsOptionsBrand = dynamicSnippetProductsOptions.extend({
+
+//     init: function () {
+//         this._super.apply(this, arguments);
+//         this.productBrands = {};   
+//     },
+
+//     // -------------------------------
+//     // Fetch marcas
+//     // -------------------------------
+//     _fetchProductBrands: function () {
+//         return this.orm.searchRead("product.attribute.value", [["attribute_id.dr_is_brand", "=", true]], ["id", "name", "attribute_id"]);
+//     },
+//     // _fetchProductBrands: function () {
+//     //     return this.orm.searchRead("product.template.attribute.line", wUtils.websiteDomain(this), ["id", "name"]);
+//     // },
+
+//     // -------------------------------
+//     // Render marcas
+//     // -------------------------------
+//     async _renderCustomXML(uiFragment) {
+//         await this._super.apply(this, arguments);
+//         await this._renderProductBrandSelector(uiFragment);  
+//     },
+
+//     async _renderProductBrandSelector(uiFragment) {
+//         const productBrands = await this._fetchProductBrands();
+//         for (let index in productBrands) {
+//             this.productBrands[productBrands[index].id] = productBrands[index];
+//         }
+//         const productBrandsSelectorEl = uiFragment.querySelector('[data-name="product_brand_opt"]');
+//         return this._renderSelectUserValueWidgetButtons(productBrandsSelectorEl, this.productBrands);
+//     },
+
+//     _setOptionsDefaultValues: function () {
+//         this._super.apply(this, arguments);
+//         this._setOptionValue('productBrandId', 'all');   
+//     },
+// });
+
+// options.registry.dynamic_snippet_products = dynamicSnippetProductsOptionsBrand;
+
+// export default dynamicSnippetProductsOptionsBrand;
