23d17f280250336a14bd559b1be7be794c121933
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-10 10:11:52 -0500

    fix-update: ...

--- conedera_theme_common/static/src/snippets/s_dynamic_snippet_brand/000.js
+++ conedera_theme_common/static/src/snippets/s_dynamic_snippet_brand/000.js
@@ -1,147 +1,243 @@
 /** @odoo-module **/
 
 import publicWidget from "@web/legacy/js/public/public_widget";
-import DynamicSnippetProducts from "website.snippets.s_dynamic_snippet_products.000";
+import { rpc } from "@web/core/network/rpc";
+import DynamicSnippetCarousel from "@website/snippets/s_dynamic_snippet_carousel/000";
+import wSaleUtils from "@website_sale/js/website_sale_utils";
+import { WebsiteSale } from "../../js/website_sale";
 
-const DynamicSnippetProductsBrand = DynamicSnippetProducts.extend({
+const DynamicSnippetProducts = DynamicSnippetCarousel.extend({
+    selector: '.s_dynamic_snippet_products',
 
-    _getSearchDomain: function () {
-        const searchDomain = this._super.apply(this, arguments);
-        const el = this.$el.get(0);
-        const productBrandId = el && el.dataset ? el.dataset.productBrandId : null;
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
 
-        console.log(">>> _getSearchDomain brandId:", productBrandId);
-
-        if (productBrandId && productBrandId !== 'all') {
-            searchDomain.push(['dr_brand_value_id', '=', parseInt(productBrandId)]);
+    /**
+     * Gets the category search domain
+     *
+     * @private
+     */
+    _getCategorySearchDomain() {
+        const searchDomain = [];
+        let productCategoryId = this.$el.get(0).dataset.productCategoryId;
+        if (productCategoryId && productCategoryId !== 'all') {
+            if (productCategoryId === 'current') {
+                productCategoryId = undefined;
+                const productCategoryField = $("#product_details").find(".product_category_id");
+                if (productCategoryField && productCategoryField.length) {
+                    productCategoryId = parseInt(productCategoryField[0].value);
+                }
+                if (!productCategoryId) {
+                    this.trigger_up('main_object_request', {
+                        callback: function (value) {
+                            if (value.model === "product.public.category") {
+                                productCategoryId = value.id;
+                            }
+                        },
+                    });
+                }
+                if (!productCategoryId) {
+                    const productTemplateId = $("#product_details").find(".product_template_id");
+                    if (productTemplateId && productTemplateId.length) {
+                        searchDomain.push(['public_categ_ids.product_tmpl_ids', '=', parseInt(productTemplateId[0].value)]);
+                    }
+                }
+            }
+            if (productCategoryId) {
+                searchDomain.push(['public_categ_ids', 'child_of', parseInt(productCategoryId)]);
+            }
         }
-
-        console.log(">>> searchDomain final:", searchDomain);
         return searchDomain;
     },
 
-    _getSearchContext: function () {
-        const searchContext = this._super.apply(this, arguments);
-        const el = this.$el.get(0);
-        const productBrandId = el && el.dataset ? el.dataset.productBrandId : null;
-
-        console.log(">>> _getSearchContext brandId:", productBrandId);
-
-        if (productBrandId && productBrandId !== 'all') {
-            searchContext.product_brand_id = parseInt(productBrandId);
+    /**
+     * Gets the tag search domain
+     *
+     * @private
+     */
+    _getTagSearchDomain() {
+        const searchDomain = [];
+        let productTagIds = this.$el.get(0).dataset.productTagIds;
+        productTagIds = productTagIds ? JSON.parse(productTagIds) : [];
+        if (productTagIds.length) {
+            searchDomain.push(['all_product_tag_ids', 'in', productTagIds.map(productTag => productTag.id)]);
         }
-        return searchContext;
+        return searchDomain;
+    },
+
+    /**
+     * 🔹 Gets the brand search domain
+     *
+     * @private
+     */
+    _getBrandSearchDomain() {
+        const searchDomain = [];
+        let productBrandId = this.$el.get(0).dataset.productBrandId;
+        if (productBrandId && productBrandId !== 'all') {
+            searchDomain.push(['dr_brand_value_id', '=', parseInt(productBrandId)]);
+        }
+        return searchDomain;
+    },
+
+    /**
+     * Method to be overridden in child components in order to provide a search
+     * domain if needed.
+     * @override
+     * @private
+     */
+    _getSearchDomain: function () {
+        const searchDomain = this._super.apply(this, arguments);
+        searchDomain.push(...this._getCategorySearchDomain());
+        searchDomain.push(...this._getTagSearchDomain());
+        searchDomain.push(...this._getBrandSearchDomain());   // 🔹 añadimos marcas
+
+        const productNames = this.$el.get(0).dataset.productNames;
+        if (productNames) {
+            const nameDomain = [];
+            for (const productName of productNames.split(',')) {
+                if (!productName.length) {
+                    continue;
+                }
+                if (nameDomain.length) {
+                    nameDomain.unshift('|');
+                }
+                nameDomain.push(...[
+                    '|', '|', ['name', 'ilike', productName],
+                              ['default_code', '=', productName],
+                              ['barcode', '=', productName],
+                ]);
+            }
+            searchDomain.push(...nameDomain);
+        }
+        if (!this.el.dataset.showVariants) {
+            searchDomain.push('hide_variants')
+        }
+        return searchDomain;
+    },
+
+    _getRpcParameters: function () {
+        const productTemplateId = $("#product_details").find(".product_template_id");
+        return Object.assign(this._super.apply(this, arguments), {
+            productTemplateId: productTemplateId && productTemplateId.length ? productTemplateId[0].value : undefined,
+        });
+    },
+
+    _getMainPageUrl() {
+        return "/shop";
     },
 });
 
-publicWidget.registry.dynamic_snippet_products = DynamicSnippetProductsBrand;
+const DynamicSnippetProductsCard = WebsiteSale.extend({
+    selector: '.o_carousel_product_card',
+    read_events: {
+        'click .js_add_cart': '_onClickAddToCart',
+        'click .js_remove': '_onRemoveFromRecentlyViewed',
+    },
 
-export default DynamicSnippetProductsBrand;
+    init(root, options) {
+        const parent = options.parent || root;
+        this._super(parent, options);
+    },
 
+    start() {
+        this.add2cartRerender = this.el.dataset.add2cartRerender === 'True';
+    },
+
+    async _onClickAddToCart(ev) {
+        const button = ev.currentTarget
+        if (!button.dataset.productSelected || button.dataset.isCombo === 'True') {
+            const dummy_form = document.createElement('form');
+            dummy_form.setAttribute('method', 'post');
+            dummy_form.setAttribute('action', '/shop/cart/update');
+
+            const inputPT = document.createElement('input');
+            inputPT.setAttribute('name', 'product_template_id');
+            inputPT.setAttribute('type', 'hidden');
+            inputPT.setAttribute('value', button.dataset.productTemplateId);
+            dummy_form.appendChild(inputPT);
+
+            const inputPP = document.createElement('input');
+            inputPP.setAttribute('name', 'product_id');
+            inputPP.setAttribute('type', 'hidden');
+            inputPP.setAttribute('value', button.dataset.productId);
+            dummy_form.appendChild(inputPP);
+
+            return this._handleAdd($(dummy_form));
+        }
+        else {
+            const data = await rpc("/shop/cart/update_json", {
+                product_id: parseInt(ev.currentTarget.dataset.productId),
+                add_qty: 1,
+                display: false,
+            });
+            wSaleUtils.updateCartNavBar(data);
+            wSaleUtils.showCartNotification(this.call.bind(this), data.notification_info);
+        }
+        if (this.add2cartRerender) {
+            this.trigger_up('widgets_start_request', {
+                $target: this.$el.closest('.s_dynamic'),
+            });
+        }
+    },
+
+    async _onRemoveFromRecentlyViewed(ev) {
+        const rpcParams = {}
+        if (ev.currentTarget.dataset.productSelected) {
+            rpcParams.product_id = ev.currentTarget.dataset.productId;
+        } else {
+            rpcParams.product_template_id = ev.currentTarget.dataset.productTemplateId;
+        }
+        await rpc("/shop/products/recently_viewed_delete", rpcParams);
+        this.trigger_up('widgets_start_request', {
+            $target: this.$el.closest('.s_dynamic'),
+        });
+    },
+});
+
+publicWidget.registry.dynamic_snippet_products_cta = DynamicSnippetProductsCard;
+publicWidget.registry.dynamic_snippet_products = DynamicSnippetProducts;
+
+export default DynamicSnippetProducts;
 
 
 // /** @odoo-module **/
 
 // import publicWidget from "@web/legacy/js/public/public_widget";
-// import { rpc } from "@web/core/network/rpc";
-// import DynamicSnippetCarousel from "@website/snippets/s_dynamic_snippet_carousel/000";
-// import wSaleUtils from "@website_sale/js/website_sale_utils";
-// import { WebsiteSale } from "../../js/website_sale";
+// import DynamicSnippetProducts from "website.snippets.s_dynamic_snippet_products.000";
 
-// const DynamicSnippetBrands = DynamicSnippetCarousel.extend({
-//     selector: '.s_dynamic_snippet_brand',
+// const DynamicSnippetProductsBrand = DynamicSnippetProducts.extend({
 
-//     //--------------------------------------------------------------------------
-//     // Private
-//     //--------------------------------------------------------------------------
+//     _getSearchDomain: function () {
+//         const searchDomain = this._super.apply(this, arguments);
+//         const el = this.$el.get(0);
+//         const productBrandId = el && el.dataset ? el.dataset.productBrandId : null;
 
-//     /**
-//      * Gets the brand search domain
-//      *
-//      * @private
-//      */
-//     _getBrandSearchDomain() {
-//         const searchDomain = [];
-//         let productBrandId = this.$el.get(0).dataset.productBrandId;
+//         console.log(">>> _getSearchDomain brandId:", productBrandId);
+
 //         if (productBrandId && productBrandId !== 'all') {
 //             searchDomain.push(['dr_brand_value_id', '=', parseInt(productBrandId)]);
 //         }
+
+//         console.log(">>> searchDomain final:", searchDomain);
 //         return searchDomain;
 //     },
 
-//     /**
-//      * @override
-//      * @private
-//      */
-//     _getSearchDomain: function () {
-//         const searchDomain = this._super.apply(this, arguments);
-//         searchDomain.push(...this._getBrandSearchDomain());
-//         return searchDomain;
-//     },
+//     _getSearchContext: function () {
+//         const searchContext = this._super.apply(this, arguments);
+//         const el = this.$el.get(0);
+//         const productBrandId = el && el.dataset ? el.dataset.productBrandId : null;
 
-//     /**
-//      * @override
-//      * @private
-//      */
-//     _getRpcParameters: function () {
-//         return Object.assign(this._super.apply(this, arguments), {
-//             product_brand_id: this.$el.get(0).dataset.productBrandId || undefined,
-            
-//         });
-//     },
+//         console.log(">>> _getSearchContext brandId:", productBrandId);
 
-//     /**
-//      * @override
-//      * @private
-//      */
-//     _getMainPageUrl() {
-//         return "/shop";
-//     },
-// });
-
-// const DynamicSnippetBrandsCard = WebsiteSale.extend({
-//     selector: '.o_carousel_product_card',
-//     read_events: {
-//         'click .js_add_cart': '_onClickAddToCart',
-//     },
-
-//     init(root, options) {
-//         const parent = options.parent || root;
-//         this._super(parent, options);
-//     },
-
-//     start() {
-//         this.add2cartRerender = this.el.dataset.add2cartRerender === 'True';
-//     },
-
-//     //--------------------------------------------------------------------------
-//     // Handlers
-//     //--------------------------------------------------------------------------
-
-//     /**
-//      * Event triggered by a click on the Add to cart button
-//      *
-//      * @param {OdooEvent} ev
-//      */
-//     async _onClickAddToCart(ev) {
-//         const button = ev.currentTarget;
-//         const data = await rpc("/shop/cart/update_json", {
-//             product_id: parseInt(button.dataset.productId),
-//             add_qty: 1,
-//             display: false,
-//         });
-//         wSaleUtils.updateCartNavBar(data);
-//         wSaleUtils.showCartNotification(this.call.bind(this), data.notification_info);
-
-//         if (this.add2cartRerender) {
-//             this.trigger_up('widgets_start_request', {
-//                 $target: this.$el.closest('.s_dynamic'),
-//             });
+//         if (productBrandId && productBrandId !== 'all') {
+//             searchContext.product_brand_id = parseInt(productBrandId);
 //         }
+//         return searchContext;
 //     },
 // });
 
-// publicWidget.registry.dynamic_snippet_brands_cta = DynamicSnippetBrandsCard;
-// publicWidget.registry.dynamic_snippet_brand = DynamicSnippetBrands;
+// publicWidget.registry.dynamic_snippet_products = DynamicSnippetProductsBrand;
 
-// export default DynamicSnippetBrands;
+// export default DynamicSnippetProductsBrand;
+
--- conedera_theme_common/static/src/snippets/s_dynamic_snippet_brand/option.js
+++ conedera_theme_common/static/src/snippets/s_dynamic_snippet_brand/option.js
@@ -8,49 +8,64 @@ const dynamicSnippetProductsOptions = s_dynamic_snippet_carousel_options.extend(
 
     init: function () {
         this._super.apply(this, arguments);
-        this.productBrands = {};
+        this.productBrands = {};   // 🔹 Diccionario para marcas
         this.orm = this.bindService("orm");
     },
 
+    //--------------------------------------------------------------------------
+    // 🔹 Marcas
+    //--------------------------------------------------------------------------
+
+    /**
+     * Trae las marcas desde product.attribute.value
+     * Solo aquellas cuyo atributo padre tiene dr_is_brand = true
+     */
     _fetchProductBrands: function () {
         return this.orm.searchRead(
-            "product.product",
-            wUtils.websiteDomain(this),
-            ["dr_brand_value_id"]
+            "product.attribute.value",
+            [["attribute_id.dr_is_brand", "=", true]],
+            ["id", "name", "display_name", "dr_image"]
         );
     },
 
-    async _renderCustomXML(uiFragment) {
-        await this._super.apply(this, arguments);
-        await this._renderProductBrandSelector(uiFragment);
-    },
-
-    async _renderProductBrandSelector(uiFragment) {
-        const products = await this._fetchProductBrands();
-        for (let product of products) {
-            if (product.dr_brand_value_id) {
-                const [id, name] = product.dr_brand_value_id;
-                if (!this.productBrands[id]) {
-                    this.productBrands[id] = { id, name };
-                }
-            }
+    /**
+     * Renderiza el selector de marcas en el editor
+     */
+    _renderProductBrandSelector: async function (uiFragment) {
+        const productBrands = await this._fetchProductBrands();
+        for (let brand of productBrands) {
+            this.productBrands[brand.id] = brand;
         }
         const productBrandsSelectorEl = uiFragment.querySelector('[data-name="product_brand_opt"]');
         return this._renderSelectUserValueWidgetButtons(productBrandsSelectorEl, this.productBrands);
     },
 
+    //--------------------------------------------------------------------------
+    // Renderizado general
+    //--------------------------------------------------------------------------
+
+    _renderCustomXML: async function (uiFragment) {
+        await this._super.apply(this, arguments);
+        await this._renderProductBrandSelector(uiFragment);  
+    },
+
+    //--------------------------------------------------------------------------
+    // Valores por defecto
+    //--------------------------------------------------------------------------
+
     _setOptionsDefaultValues: function () {
-        this._setOptionValue('productBrandId', 'all');
+        this._setOptionValue('productBrandId', 'all');   
         this._super.apply(this, arguments);
     },
 
+    /**
+     * Guardar selección en el DOM y actualizar dominio contextual
+     */
     _setOptionValue: function (optionName, value) {
         this._super.apply(this, arguments);
         if (optionName === 'productBrandId') {
-            // Guardar en el DOM
             this.$target[0].dataset.productBrandId = value;
 
-            // Limpiar entradas previas de marca en el dominio contextual
             if (this.contextualFilterDomain) {
                 this.contextualFilterDomain = this.contextualFilterDomain.filter(
                     (c) => !(Array.isArray(c) && c[0] === 'dr_brand_value_id')
@@ -59,7 +74,6 @@ const dynamicSnippetProductsOptions = s_dynamic_snippet_carousel_options.extend(
                 this.contextualFilterDomain = [];
             }
 
-            // Añadir nueva marca seleccionada
             if (value && value !== 'all') {
                 this.contextualFilterDomain.push(['dr_brand_value_id', '=', parseInt(value)]);
             }
@@ -70,3 +84,76 @@ const dynamicSnippetProductsOptions = s_dynamic_snippet_carousel_options.extend(
 options.registry.dynamic_snippet_products = dynamicSnippetProductsOptions;
 
 export default dynamicSnippetProductsOptions;
+
+// /** @odoo-module **/
+
+// import options from "@web_editor/js/editor/snippets.options";
+// import s_dynamic_snippet_carousel_options from "@website/snippets/s_dynamic_snippet_carousel/options";
+// import wUtils from "@website/js/utils";
+
+// const dynamicSnippetProductsOptions = s_dynamic_snippet_carousel_options.extend({
+
+//     init: function () {
+//         this._super.apply(this, arguments);
+//         this.productBrands = {};
+//         this.orm = this.bindService("orm");
+//     },
+
+//     _fetchProductBrands: function () {
+//         return this.orm.searchRead(
+//             "product.product",
+//             wUtils.websiteDomain(this),
+//             ["dr_brand_value_id"]
+//         );
+//     },
+
+//     async _renderCustomXML(uiFragment) {
+//         await this._super.apply(this, arguments);
+//         await this._renderProductBrandSelector(uiFragment);
+//     },
+
+//     async _renderProductBrandSelector(uiFragment) {
+//         const products = await this._fetchProductBrands();
+//         for (let product of products) {
+//             if (product.dr_brand_value_id) {
+//                 const [id, name] = product.dr_brand_value_id;
+//                 if (!this.productBrands[id]) {
+//                     this.productBrands[id] = { id, name };
+//                 }
+//             }
+//         }
+//         const productBrandsSelectorEl = uiFragment.querySelector('[data-name="product_brand_opt"]');
+//         return this._renderSelectUserValueWidgetButtons(productBrandsSelectorEl, this.productBrands);
+//     },
+
+//     _setOptionsDefaultValues: function () {
+//         this._setOptionValue('productBrandId', 'all');
+//         this._super.apply(this, arguments);
+//     },
+
+//     _setOptionValue: function (optionName, value) {
+//         this._super.apply(this, arguments);
+//         if (optionName === 'productBrandId') {
+//             // Guardar en el DOM
+//             this.$target[0].dataset.productBrandId = value;
+
+//             // Limpiar entradas previas de marca en el dominio contextual
+//             if (this.contextualFilterDomain) {
+//                 this.contextualFilterDomain = this.contextualFilterDomain.filter(
+//                     (c) => !(Array.isArray(c) && c[0] === 'dr_brand_value_id')
+//                 );
+//             } else {
+//                 this.contextualFilterDomain = [];
+//             }
+
+//             // Añadir nueva marca seleccionada
+//             if (value && value !== 'all') {
+//                 this.contextualFilterDomain.push(['dr_brand_value_id', '=', parseInt(value)]);
+//             }
+//         }
+//     },
+// });
+
+// options.registry.dynamic_snippet_products = dynamicSnippetProductsOptions;
+
+// export default dynamicSnippetProductsOptions;
