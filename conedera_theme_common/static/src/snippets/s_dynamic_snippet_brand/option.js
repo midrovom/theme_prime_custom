e06556c12a51b4c315b9217bb1fb0e07af888857
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-10 15:09:46 -0500

    fix-update: ...

--- conedera_theme_common/static/src/snippets/s_dynamic_snippet_brand/option.js
+++ conedera_theme_common/static/src/snippets/s_dynamic_snippet_brand/option.js
@@ -7,41 +7,53 @@ options.registry.dynamic_snippet_products.include({
     init: function () {
         this._super.apply(this, arguments);
         this.productBrands = {};
+        console.log("[dynamic_snippet_products] init ejecutado");
     },
 
     // -------------------------------
     // Fetch marcas
     // -------------------------------
     _fetchProductBrands: function () {
+        console.log("[dynamic_snippet_products] _fetchProductBrands llamado");
         return this.orm.searchRead(
             "product.attribute.value",
             [["attribute_id.dr_is_brand", "=", true]],
             ["id", "name", "attribute_id"]
-        );
+        ).then(result => {
+            console.log("[dynamic_snippet_products] _fetchProductBrands resultado:", result);
+            return result;
+        });
     },
 
     // -------------------------------
     // Render marcas
     // -------------------------------
     async _renderCustomXML(uiFragment) {
+        console.log("[dynamic_snippet_products] _renderCustomXML llamado");
         await this._super.apply(this, arguments);
         await this._renderProductBrandSelector(uiFragment);
     },
 
     async _renderProductBrandSelector(uiFragment) {
+        console.log("[dynamic_snippet_products] _renderProductBrandSelector llamado");
         const productBrands = await this._fetchProductBrands();
         for (let index in productBrands) {
             this.productBrands[productBrands[index].id] = productBrands[index];
         }
+        console.log("[dynamic_snippet_products] _renderProductBrandSelector marcas procesadas:", this.productBrands);
         const productBrandsSelectorEl = uiFragment.querySelector('[data-name="product_brand_opt"]');
+        console.log("[dynamic_snippet_products] Selector encontrado:", productBrandsSelectorEl);
         return this._renderSelectUserValueWidgetButtons(productBrandsSelectorEl, this.productBrands);
     },
 
     _setOptionsDefaultValues: function () {
+        console.log("[dynamic_snippet_products] _setOptionsDefaultValues llamado");
         this._super.apply(this, arguments);
         this._setOptionValue('productBrandId', 'all');
+        console.log("[dynamic_snippet_products] Valor por defecto productBrandId = all");
     },
 });
+
 
 // /** @odoo-module **/
 
