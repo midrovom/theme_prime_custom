7935514101af43b4a8355f3f9541152d0038894f
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-10 16:11:14 -0500

    fix-update: ...

--- conedera_theme_common/controller/main.py
+++ conedera_theme_common/controller/main.py
@@ -1,14 +1,34 @@
-from odoo import http
-from odoo.http import request
-from odoo.addons.website_sale.controllers.snippets import WebsiteSaleSnippetProducts
+from odoo.addons.website_sale.controllers.main import WebsiteSale
 
-class WebsiteSaleSnippetProductsBrand(WebsiteSaleSnippetProducts):
+class WebsiteSaleBrand(WebsiteSale):
+
+    def _get_search_options(
+        self, category=None, attrib_values=None, tags=None,
+        min_price=0.0, max_price=0.0, conversion_rate=1, **post
+    ):
+        # Llamamos al original
+        options = super()._get_search_options(
+            category=category,
+            attrib_values=attrib_values,
+            tags=tags,
+            min_price=min_price,
+            max_price=max_price,
+            conversion_rate=conversion_rate,
+            **post
+        )
+
+        # 👇 añadimos nuestra opción de marca
+        product_brand_id = post.get('productBrandId')
+        if product_brand_id and product_brand_id != 'all':
+            options['productBrandId'] = product_brand_id
+
+        return options
 
     def _get_search_domain(self, options):
         # Llamamos al original
         domain = super()._get_search_domain(options)
 
-        # Añadimos filtro por marca si existe
+        # añadimos el filtro por marca
         product_brand_id = options.get('productBrandId')
         if product_brand_id and product_brand_id != 'all':
             domain.append(('dr_brand_value_id', '=', int(product_brand_id)))
