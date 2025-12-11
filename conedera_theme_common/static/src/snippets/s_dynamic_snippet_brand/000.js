9d615c5a32b273cf79890dd784fef1ea44b1de65
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-11 10:32:22 -0500

    fix-update: ...

--- conedera_theme_common/static/src/snippets/s_dynamic_snippet_brand/000.js
+++ conedera_theme_common/static/src/snippets/s_dynamic_snippet_brand/000.js
@@ -4,20 +4,15 @@ import publicWidget from "@web/legacy/js/public/public_widget";
 
 publicWidget.registry.dynamic_snippet_products.include({
 
-    /**
-     * Extiende el dominio original del snippet para añadir el filtro de marca
-     */
     _getSearchDomain: function () {
-        console.log("[DynamicSnippetProductsBrand] _getSearchDomain ejecutado");
         const searchDomain = this._super.apply(this, arguments);
-        console.log("[DynamicSnippetProductsBrand] Dominio original:", searchDomain);
-
         const productBrandId = this.$el.get(0).dataset.productBrandId;
+
         if (productBrandId && productBrandId !== 'all') {
             searchDomain.push(['dr_brand_value_id', '=', parseInt(productBrandId)]);
         }
 
-        console.log("[DynamicSnippetProductsBrand] Dominio final con marca:", searchDomain);
+        console.log("[DynamicSnippetProductsBrand] Dominio final:", searchDomain);
         return searchDomain;
     },
 });
