879dadcc7d87dc8047b256039c22ba7803a1a44f
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-11 10:56:59 -0500

    fix-update: ...

--- conedera_theme_common/static/src/snippets/s_dynamic_snippet_brand/000.js
+++ conedera_theme_common/static/src/snippets/s_dynamic_snippet_brand/000.js
@@ -1,17 +1,42 @@
 /** @odoo-module **/
 
 import publicWidget from "@web/legacy/js/public/public_widget";
+import DynamicSnippetProducts from "@website/snippets/s_dynamic_snippet_carousel/000";
 
-publicWidget.registry.dynamic_snippet_products.include({
+const DynamicSnippetProductsBrand = DynamicSnippetProducts.extend({
+
+    /**
+     * Construye el dominio de búsqueda para marcas
+     */
+
     _getSearchDomain: function () {
+        console.log("[DynamicSnippetProductsBrand] _getSearchDomain ejecutado");
         const searchDomain = this._super.apply(this, arguments);
-        const productBrandId = this.$el.get(0).dataset.productBrandId;
+        console.log("[DynamicSnippetProductsBrand] Dominio original:", searchDomain);
 
-        if (productBrandId && productBrandId !== 'all') {
-            searchDomain.push(['product_tmpl_id.attribute_line_ids.value_ids', 'in', [parseInt(productBrandId)]]);
-        }
+        const brandDomain = this._getBrandSearchDomain();
+        searchDomain.push(...brandDomain);
 
+        console.log("[DynamicSnippetProductsBrand] Dominio final con marca:", searchDomain);
+        return searchDomain;
+    },
+
+    /**
+     * Extiende el dominio original del snippet para añadir el filtro de marca
+     */
+    _getSearchDomain: function () {
+        console.log("[DynamicSnippetProductsBrand] _getSearchDomain ejecutado");
+        const searchDomain = this._super.apply(this, arguments);
+        console.log("[DynamicSnippetProductsBrand] Dominio original:", searchDomain);
+
+        const brandDomain = this._getBrandSearchDomain();
+        searchDomain.push(...brandDomain);
+
+        console.log("[DynamicSnippetProductsBrand] Dominio final con marca:", searchDomain);
         return searchDomain;
     },
 });
 
+publicWidget.registry.dynamic_snippet_products_brand = DynamicSnippetProductsBrand;
+
+export default DynamicSnippetProductsBrand;
