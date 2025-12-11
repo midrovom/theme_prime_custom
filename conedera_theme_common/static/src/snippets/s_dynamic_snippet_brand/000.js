16d0f2a957c4ac6b1fa39cda18cb249a5eb39341
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-11 10:20:23 -0500

    fix-update: ...

--- conedera_theme_common/static/src/snippets/s_dynamic_snippet_brand/000.js
+++ conedera_theme_common/static/src/snippets/s_dynamic_snippet_brand/000.js
@@ -1,25 +1,8 @@
 /** @odoo-module **/
 
 import publicWidget from "@web/legacy/js/public/public_widget";
-import DynamicSnippetProducts from "@website/snippets/s_dynamic_snippet_carousel/000";
 
-const DynamicSnippetProductsBrand = DynamicSnippetProducts.extend({
-
-    /**
-     * Construye el dominio de búsqueda para marcas
-     */
-
-    _getSearchDomain: function () {
-        console.log("[DynamicSnippetProductsBrand] _getSearchDomain ejecutado");
-        const searchDomain = this._super.apply(this, arguments);
-        console.log("[DynamicSnippetProductsBrand] Dominio original:", searchDomain);
-
-        const brandDomain = this._getBrandSearchDomain();
-        searchDomain.push(...brandDomain);
-
-        console.log("[DynamicSnippetProductsBrand] Dominio final con marca:", searchDomain);
-        return searchDomain;
-    },
+publicWidget.registry.dynamic_snippet_products.include({
 
     /**
      * Extiende el dominio original del snippet para añadir el filtro de marca
@@ -29,14 +12,12 @@ const DynamicSnippetProductsBrand = DynamicSnippetProducts.extend({
         const searchDomain = this._super.apply(this, arguments);
         console.log("[DynamicSnippetProductsBrand] Dominio original:", searchDomain);
 
-        const brandDomain = this._getBrandSearchDomain();
-        searchDomain.push(...brandDomain);
+        const productBrandId = this.$el.get(0).dataset.productBrandId;
+        if (productBrandId && productBrandId !== 'all') {
+            searchDomain.push(['dr_brand_value_id', '=', parseInt(productBrandId)]);
+        }
 
         console.log("[DynamicSnippetProductsBrand] Dominio final con marca:", searchDomain);
         return searchDomain;
     },
 });
-
-publicWidget.registry.dynamic_snippet_products_brand = DynamicSnippetProductsBrand;
-
-export default DynamicSnippetProductsBrand;
