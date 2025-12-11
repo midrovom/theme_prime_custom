ec1834aa77e43c2679c612cf4ca49d470c65e838
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-11 10:11:09 -0500

    fix-update: ...

--- conedera_theme_common/static/src/snippets/s_dynamic_snippet_brand/000.js
+++ conedera_theme_common/static/src/snippets/s_dynamic_snippet_brand/000.js
@@ -9,12 +9,15 @@ const DynamicSnippetProductsBrand = DynamicSnippetProducts.extend({
      * Construye el dominio de búsqueda para marcas
      */
 
-    _getBrandSearchDomain() {
-        const searchDomain = [];
-        let productBrandId = this.$el.get(0).dataset.productBrandId;
-        if (productBrandId && productBrandId !== 'all') {
-            searchDomain.push(['dr_brand_value_id', '=', parseInt(productBrandId)]);
-        }
+    _getSearchDomain: function () {
+        console.log("[DynamicSnippetProductsBrand] _getSearchDomain ejecutado");
+        const searchDomain = this._super.apply(this, arguments);
+        console.log("[DynamicSnippetProductsBrand] Dominio original:", searchDomain);
+
+        const brandDomain = this._getBrandSearchDomain();
+        searchDomain.push(...brandDomain);
+
+        console.log("[DynamicSnippetProductsBrand] Dominio final con marca:", searchDomain);
         return searchDomain;
     },
 
