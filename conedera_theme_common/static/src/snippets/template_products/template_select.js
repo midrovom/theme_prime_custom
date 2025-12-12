c838d782640e02c920dc63b3113b7f063c7a05df
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-12 16:15:54 -0500

    fix-update: ...

--- conedera_theme_common/static/src/snippets/template_products/template_select.js
+++ conedera_theme_common/static/src/snippets/template_products/template_select.js
@@ -1,21 +1,15 @@
 /** @odoo-module **/
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll(".o_desc_product_snippet .desc-header")
-        .forEach(function (header) {
-            
-            header.addEventListener("click", function () {
+        .forEach(header => {
+            header.addEventListener("click", () => {
                 const container = header.closest(".o_desc_product_snippet");
                 const body = container.querySelector(".desc-body");
                 const icon = container.querySelector(".desc-icon");
 
                 body.classList.toggle("d-none");
-
-                if (body.classList.contains("d-none")) {
-                    icon.style.transform = "rotate(0deg)";
-                } else {
-                    icon.style.transform = "rotate(180deg)";
-                }
+                icon.style.transform = body.classList.contains("d-none") ? "rotate(0deg)" : "rotate(180deg)";
             });
         });
 });
