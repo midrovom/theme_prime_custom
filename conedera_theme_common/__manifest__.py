ab2fdeb949d01dfebf26f141db8f572bc344303a
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-12 14:49:44 -0500

    fix-update: ...

--- conedera_theme_common/__manifest__.py
+++ conedera_theme_common/__manifest__.py
@@ -24,8 +24,9 @@
         'views/snippets/s_dynamic_snippet_brands.xml',
         'views/snippets/snippets.xml',
         'views/snippets/s_key_images_custom.xml',
-        'views/snippets/s_text.xml',
         'views/templates_productos.xml',
+        'views/snippets/s_text.xml',
+
         
     ],
     'auto_install': False,
