8f18a3d30d3c08da26a58c17c18591c013264ee6
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-15 12:43:47 -0500

    fix-update: ...

--- conedera_theme_common/controllers/main.py
+++ conedera_theme_common/controllers/main.py
@@ -1,21 +1,24 @@
-# from odoo.addons.website_sale.controllers.main import WebsiteSale
+import logging
+_logger = logging.getLogger(__name__)
 
-# class WebsiteSaleExtended(WebsiteSale):
+class WebsiteSaleExtended(WebsiteSale):
 
-#     def _prepare_product_data(self, products, fields, pricelist, options=None):
-#         fields = list(set(fields or []))
-#         fields += ['attribute_line_ids']
-#         result = super()._prepare_product_data(products, fields, pricelist, options)
+    def _prepare_product_data(self, products, fields, pricelist, options=None):
+        fields = list(set(fields or []))
+        fields += ['attribute_line_ids']
+        result = super()._prepare_product_data(products, fields, pricelist, options)
 
-#         for res_product, product in zip(result, products):
-#             res_product['attributes'] = []
-#             for line in product.attribute_line_ids:
-#                 if line.attribute_id.attribute_custom:
-#                     for val in line.value_ids:
-#                         res_product['attributes'].append({
-#                             'id': val.id,
-#                             'name': val.name,
-#                             'attribute_name': line.attribute_id.name,
-#                             'image': val.image_1920 and f'/web/image/product.attribute.value/{val.id}/image_1920' or False,
-#                         })
-#         return result
+        for res_product, product in zip(result, products):
+            res_product['attributes'] = []
+            for line in product.attribute_line_ids:
+                if line.attribute_id.attribute_custom:
+                    for val in line.value_ids:
+                        attr_data = {
+                            'id': val.id,
+                            'name': val.name,
+                            'attribute_name': line.attribute_id.name,
+                            'image': val.image_1920 and f'/web/image/product.attribute.value/{val.id}/image_1920' or False,
+                        }
+                        res_product['attributes'].append(attr_data)
+                        _logger.info("Producto %s - atributo añadido: %s", product.name, attr_data)
+        return result
