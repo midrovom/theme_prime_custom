32e647c08424b8794375c7addef9dcb494daadeb
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-15 12:04:35 -0500

    fix-update: ...

--- conedera_theme_common/models/__init__.py
+++ conedera_theme_common/models/__init__.py
@@ -1,5 +1,5 @@
 from . import website_snippet_filter
 from . import product_public_category
 from . import website_menu
-from . import product_attribute
+#from . import product_attribute
 #from . import product_template
