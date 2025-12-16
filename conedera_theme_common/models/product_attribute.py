6292e8ad89817f46ceaee12efbaba8f87df47504
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-16 16:22:54 -0500

    fix-update: ...

--- conedera_theme_common/models/product_attribute.py
+++ conedera_theme_common/models/product_attribute.py
@@ -4,3 +4,4 @@ class ProductAttribute(models.Model):
     _inherit = 'product.attribute'
 
     attribute_custom = fields.Boolean(string="Atributos para productos")
+    filter_attribute = fields.Boolean(string="Atributos para filtro")
