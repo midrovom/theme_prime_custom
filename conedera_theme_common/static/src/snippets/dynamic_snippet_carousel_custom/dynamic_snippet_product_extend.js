658e37a4eafa8c1e19af1cdc7a6456ca374b6165
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-17 11:25:12 -0500

    fix-update: ...

--- conedera_theme_common/static/src/snippets/dynamic_snippet_carousel_custom/dynamic_snippet_product_extend.js
+++ conedera_theme_common/static/src/snippets/dynamic_snippet_carousel_custom/dynamic_snippet_product_extend.js
@@ -4,13 +4,14 @@ import DynamicSnippetProducts from "@website_sale/snippets/s_dynamic_snippet_pro
 import { utils as uiUtils } from "@web/core/ui/ui_service";
 
 const DynamicSnippetProductsExtended = DynamicSnippetProducts.extend({
-
     _getQWebRenderOptions() {
         const options = this._super(...arguments);
 
+        // Leer el template-key directamente del DOM
         const tplKey = this.$el.data("template-key");
-        console.log("template-key desde DOM:", tplKey);
+        console.log("template-key detectado:", tplKey);
 
+        // Si es tu template y es móvil → forzar 1 producto por slide
         if (
             tplKey &&
             tplKey.includes("dynamic_filter_template_product_product_style_2") &&
@@ -23,30 +24,10 @@ const DynamicSnippetProductsExtended = DynamicSnippetProducts.extend({
 
         return options;
     },
-
-    // _getQWebRenderOptions() {
-    //     const options = this._super.apply(this, arguments);
-
-    //     console.log("TemplateKey recibido:", options.templateKey);
-
-    //     // Forzar 1 producto por slide SOLO en móvil para tu template
-    //     if (
-    //         options.templateKey &&
-    //         options.templateKey.includes("conedera_theme_common.dynamic_filter_template_product_product_style_2") &&
-    //         uiUtils.isSmall()
-    //     ) {
-    //         options.chunkSize = 1;
-    //     } else {
-    //         options.chunkSize = uiUtils.isSmall() ? 2 : 4;
-    //     }
-
-    //     return options;
-    // },
 });
 
 publicWidget.registry.dynamic_snippet_products = DynamicSnippetProductsExtended;
 export default DynamicSnippetProductsExtended;
-
 
 // /** @odoo-module **/
 // import publicWidget from "@web/legacy/js/public/public_widget";
