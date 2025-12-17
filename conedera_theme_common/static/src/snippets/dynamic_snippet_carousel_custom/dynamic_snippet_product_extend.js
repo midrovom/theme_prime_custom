b0927e1db59e7523da1fb06374b4a56e73dfb5a5
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-17 08:59:53 -0500

    fix-update: ...

--- conedera_theme_common/static/src/snippets/dynamic_snippet_carousel_custom/dynamic_snippet_product_extend.js
+++ conedera_theme_common/static/src/snippets/dynamic_snippet_carousel_custom/dynamic_snippet_product_extend.js
@@ -7,14 +7,28 @@ const DynamicSnippetProductsExtended = DynamicSnippetProducts.extend({
     /**
      * @override
      */
+
         _getQWebRenderOptions() {
         const options = this._super.apply(this, arguments);
-        if (uiUtils.isSmall()) {
-            options.chunkSize = 2; //vista de productos mobil
+        if (options.templateKey === "dynamic_filter_template_product_product_style_2") {
+            options.chunkSize = 1;
         } else {
-            options.chunkSize = 4;// cantidad de productos web/escritorio
+            if (uiUtils.isSmall()) {
+                options.chunkSize = 2;
+            } else {
+                options.chunkSize = 4;
+            }
         }
         return options;
+
+        // _getQWebRenderOptions() {
+        // const options = this._super.apply(this, arguments);
+        // if (uiUtils.isSmall()) {
+        //     options.chunkSize = 2; //vista de productos mobil
+        // } else {
+        //     options.chunkSize = 4;// cantidad de productos web/escritorio
+        // }
+        // return options;
     },
 });
 
