a939b3133d9f598f5d774ba7a8ff5a00d5a48cfe
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-17 11:16:42 -0500

    fix-update: ...

--- conedera_theme_common/static/src/snippets/dynamic_snippet_carousel_custom/dynamic_snippet_product_extend.js
+++ conedera_theme_common/static/src/snippets/dynamic_snippet_carousel_custom/dynamic_snippet_product_extend.js
@@ -4,22 +4,52 @@ import DynamicSnippetProducts from "@website_sale/snippets/s_dynamic_snippet_pro
 import { utils as uiUtils } from "@web/core/ui/ui_service";
 
 const DynamicSnippetProductsExtended = DynamicSnippetProducts.extend({
-    /**
-     * @override
-     */
-
-        _getQWebRenderOptions() {
+    _getQWebRenderOptions() {
         const options = this._super.apply(this, arguments);
-        if (uiUtils.isSmall()) {
-            options.chunkSize = 2; //vista de productos mobil
+
+        console.log("TemplateKey recibido:", options.templateKey);
+
+        // Forzar 1 producto por slide SOLO en móvil para tu template
+        if (
+            options.templateKey &&
+            options.templateKey.includes("conedera_theme_common.dynamic_filter_template_product_product_style_2") &&
+            uiUtils.isSmall()
+        ) {
+            options.chunkSize = 1;
         } else {
-            options.chunkSize = 4;// cantidad de productos web/escritorio
+            options.chunkSize = uiUtils.isSmall() ? 2 : 4;
         }
+
         return options;
     },
 });
 
-// Se sobreescribe el registro original
 publicWidget.registry.dynamic_snippet_products = DynamicSnippetProductsExtended;
-
 export default DynamicSnippetProductsExtended;
+
+
+// /** @odoo-module **/
+// import publicWidget from "@web/legacy/js/public/public_widget";
+// import DynamicSnippetProducts from "@website_sale/snippets/s_dynamic_snippet_products/000";
+// import { utils as uiUtils } from "@web/core/ui/ui_service";
+
+// const DynamicSnippetProductsExtended = DynamicSnippetProducts.extend({
+//     /**
+//      * @override
+//      */
+
+//         _getQWebRenderOptions() {
+//         const options = this._super.apply(this, arguments);
+//         if (uiUtils.isSmall()) {
+//             options.chunkSize = 2; //vista de productos mobil
+//         } else {
+//             options.chunkSize = 4;// cantidad de productos web/escritorio
+//         }
+//         return options;
+//     },
+// });
+
+// // Se sobreescribe el registro original
+// publicWidget.registry.dynamic_snippet_products = DynamicSnippetProductsExtended;
+
+// export default DynamicSnippetProductsExtended;
