bae39072fe74ae685ef4cf1a60490161ca4d8a88
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-17 11:36:02 -0500

    fix-update: ...

--- conedera_theme_common/static/src/snippets/dynamic_snippet_carousel_custom/dynamic_snippet_product_extend.js
+++ conedera_theme_common/static/src/snippets/dynamic_snippet_carousel_custom/dynamic_snippet_product_extend.js
@@ -28,29 +28,3 @@ const DynamicSnippetProductsExtended = DynamicSnippetProducts.extend({
 
 publicWidget.registry.dynamic_snippet_products = DynamicSnippetProductsExtended;
 export default DynamicSnippetProductsExtended;
-
-// /** @odoo-module **/
-// import publicWidget from "@web/legacy/js/public/public_widget";
-// import DynamicSnippetProducts from "@website_sale/snippets/s_dynamic_snippet_products/000";
-// import { utils as uiUtils } from "@web/core/ui/ui_service";
-
-// const DynamicSnippetProductsExtended = DynamicSnippetProducts.extend({
-//     /**
-//      * @override
-//      */
-
-//         _getQWebRenderOptions() {
-//         const options = this._super.apply(this, arguments);
-//         if (uiUtils.isSmall()) {
-//             options.chunkSize = 2; //vista de productos mobil
-//         } else {
-//             options.chunkSize = 4;// cantidad de productos web/escritorio
-//         }
-//         return options;
-//     },
-// });
-
-// // Se sobreescribe el registro original
-// publicWidget.registry.dynamic_snippet_products = DynamicSnippetProductsExtended;
-
-// export default DynamicSnippetProductsExtended;
