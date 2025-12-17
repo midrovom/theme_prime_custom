2a284c281e601e1d473e55cff0cea44588e4547c
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-17 09:09:46 -0500

    fix-update: ...

--- conedera_theme_common/static/src/snippets/dynamic_snippet_carousel_custom/dynamic_snippet_product_extend.js
+++ conedera_theme_common/static/src/snippets/dynamic_snippet_carousel_custom/dynamic_snippet_product_extend.js
@@ -7,32 +7,66 @@ const DynamicSnippetProductsExtended = DynamicSnippetProducts.extend({
     /**
      * @override
      */
-
-        _getQWebRenderOptions() {
+    _getQWebRenderOptions() {
         const options = this._super.apply(this, arguments);
+
+        // Forzar chunkSize = 1 SOLO para el template "dynamic_filter_template_product_product_style_2"
         if (options.templateKey === "dynamic_filter_template_product_product_style_2") {
             options.chunkSize = 1;
         } else {
+            // Para los demás templates, mantener la lógica normal
             if (uiUtils.isSmall()) {
-                options.chunkSize = 2;
+                options.chunkSize = 2; // vista móvil
             } else {
-                options.chunkSize = 4;
+                options.chunkSize = 4; // vista escritorio
             }
         }
         return options;
-
-        // _getQWebRenderOptions() {
-        // const options = this._super.apply(this, arguments);
-        // if (uiUtils.isSmall()) {
-        //     options.chunkSize = 2; //vista de productos mobil
-        // } else {
-        //     options.chunkSize = 4;// cantidad de productos web/escritorio
-        // }
-        // return options;
     },
 });
 
-// Se sobreescribe el registro original
+// Sobrescribir el registro original
 publicWidget.registry.dynamic_snippet_products = DynamicSnippetProductsExtended;
 
 export default DynamicSnippetProductsExtended;
+
+
+// /** @odoo-module **/
+// import publicWidget from "@web/legacy/js/public/public_widget";
+// import DynamicSnippetProducts from "@website_sale/snippets/s_dynamic_snippet_products/000";
+// import { utils as uiUtils } from "@web/core/ui/ui_service";
+
+// const DynamicSnippetProductsExtended = DynamicSnippetProducts.extend({
+//     /**
+//      * @override
+//      */
+
+//     _getQWebRenderOptions() {
+//     const options = this._super.apply(this, arguments);
+//     if (options.templateKey === "dynamic_filter_template_product_product_style_2") {
+//         options.chunkSize = 1;
+//     } else {
+//         if (uiUtils.isSmall()) {
+//             options.chunkSize = 2;
+//         } else {
+//             options.chunkSize = 4;
+//         }
+//     }
+//     return options;
+
+
+//         // _getQWebRenderOptions() {
+//         // const options = this._super.apply(this, arguments);
+//         // if (uiUtils.isSmall()) {
+//         //     options.chunkSize = 2; //vista de productos mobil
+//         // } else {
+//         //     options.chunkSize = 4;// cantidad de productos web/escritorio
+//         // }
+//         // return options;
+//     },
+// });
+
+// // Se sobreescribe el registro original
+// publicWidget.registry.dynamic_snippet_products = DynamicSnippetProductsExtended;
+
+// export default DynamicSnippetProductsExtended;
