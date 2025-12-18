9f182a3fa17110f5b7fbb77f736d8f6c8fd3b6bb
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-18 16:05:19 -0500

    fix-update: ...

--- conedera_theme_common/static/src/snippets/s_dynamic_snippet_brand/000.js
+++ conedera_theme_common/static/src/snippets/s_dynamic_snippet_brand/000.js
@@ -1,10 +1,30 @@
 /** @odoo-module **/
-
 import publicWidget from "@web/legacy/js/public/public_widget";
 import DynamicSnippetProducts from "@website_sale/snippets/s_dynamic_snippet_products/000";
+import { utils as uiUtils } from "@web/core/ui/ui_service";
 
-const DynamicSnippetProductsBrand = DynamicSnippetProducts.extend({
+const DynamicSnippetProductsCombined = DynamicSnippetProducts.extend({
 
+    // Ajuste de chunkSize según template y tamaño de pantalla
+    _getQWebRenderOptions() {
+        const options = this._super(...arguments);
+
+        const tplKey = this.$el.data("template-key");
+
+        if (
+            tplKey &&
+            tplKey.includes("dynamic_filter_template_product_product_style_2") &&
+            uiUtils.isSmall()
+        ) {
+            options.chunkSize = 1;
+        } else {
+            options.chunkSize = uiUtils.isSmall() ? 2 : 4;
+        }
+
+        return options;
+    },
+
+    // Filtro por marca
     _getSearchDomain() {
         const domain = this._super(...arguments);
         const brandDomain = this._getBrandSearchDomain();
@@ -21,6 +41,32 @@ const DynamicSnippetProductsBrand = DynamicSnippetProducts.extend({
     },
 });
 
-publicWidget.registry.dynamic_snippet_products = DynamicSnippetProductsBrand;
+publicWidget.registry.dynamic_snippet_products = DynamicSnippetProductsCombined;
+export default DynamicSnippetProductsCombined;
 
-export default DynamicSnippetProductsBrand;
+// /** @odoo-module **/
+
+// import publicWidget from "@web/legacy/js/public/public_widget";
+// import DynamicSnippetProducts from "@website_sale/snippets/s_dynamic_snippet_products/000";
+
+// const DynamicSnippetProductsBrand = DynamicSnippetProducts.extend({
+
+//     _getSearchDomain() {
+//         const domain = this._super(...arguments);
+//         const brandDomain = this._getBrandSearchDomain();
+//         domain.push(...brandDomain);
+//         return domain;
+//     },
+
+//     _getBrandSearchDomain() {
+//         let brandId = this.$el.get(0).dataset.productBrandId;
+//         if (!brandId || brandId === "all") {
+//             return [];
+//         }
+//         return [["attribute_line_ids.value_ids", "in", [parseInt(brandId)]]];
+//     },
+// });
+
+// publicWidget.registry.dynamic_snippet_products = DynamicSnippetProductsBrand;
+
+// export default DynamicSnippetProductsBrand;
