dc70a2333f21ebfdf1eca8b2d9ebf701f98320b6
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-22 11:33:55 -0500

    fix-update: ...

--- advance_theme_common/__manifest__.py
+++ advance_theme_common/__manifest__.py
@@ -21,6 +21,7 @@
             'advance_theme_common/static/src/scss/s_banner/s_banner_17.scss',
             'advance_theme_common/static/src/scss/s_cover/s_cover_6.scss',
             'advance_theme_common/static/src/scss/s_shop_offert/s_shop_offer_6.scss',
+            'advance_theme_common/static/src/scss/s_shop_offert/shop_offert_7.scss',
             # 'advance_theme_custom/static/src/scss/gallery_custom/gallery_custom.scss',
             # 'advance_theme_custom/static/src/scss/texto_custom/texto_custom.scss',
             # 'advance_theme_custom/static/src/scss/banner_custom/custom_category_slider.scss',
