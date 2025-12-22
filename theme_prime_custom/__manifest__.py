bfffeae96de3f5092325ae9207d2c1a4c69ae4a1
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-12-22 09:34:31 -0500

    fix-update: ...

---
+++ theme_prime_custom/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Theme Prime Custom',
+    'version': '18.0.1.0.0',
+    'summary': 'Temas y snippets personalizados para Conedera y Advance',
+    'category': 'Theme/eCommerce',
+    'depends': ['website', 'theme_prime', 'conedera_theme_common'],
+    'data': [
+        # Snippet Advance
+        'views/advance/snippets_register_advance.xml',
+        'views/advance/snippet/banner/s_banner_9_custom.xml',
+
+    ],
+    'assets': {
+        'web.assets_frontend': [
+
+    ],
+        'website.assets_wysiwyg': [
+        
+        ],
+    },
+    'installable': True,
+    'application': False,
+    'auto_install': False
+}
