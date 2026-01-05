87b056ea28eebc246b33b7e7b0755eeb6fd07425
Diego Arcos oficce <darcos@callphoneecuador.com>
2026-01-05 12:16:20 -0500

    fix-update: ...

--- theme_prime_custom/__manifest__.py
+++ theme_prime_custom/__manifest__.py
@@ -6,22 +6,25 @@
     'depends': ['website', 'theme_prime', 'conedera_theme_common'],
     'data': [
         # Snippet Advance
-        'views/advance/snippets_register_advance.xml',
-        'views/advance/snippet/s_banner/s_banner_9_custom.xml',
-        'views/advance/snippet/s_cover/s_cover_6.xml',
-        'views/advance/snippet/s_shop_offer/s_shop_offer_6.xml',
-        'views/advance/snippet/s_banner/s_banner_17.xml',
-        'views/advance/snippet/s_shop_offer/s_shop_offer_7.xml',
-        'views/advance/snippet/s_text/s_text.xml',
-        'views/advance/snippet/s_footer/s_footer.xml',
-        'views/advance/snippet/s_header/s_header.xml',
+        # 'views/advance/snippets_register_advance.xml',
+        # 'views/advance/snippet/s_banner/s_banner_9_custom.xml',
+        # 'views/advance/snippet/s_cover/s_cover_6.xml',
+        # 'views/advance/snippet/s_shop_offer/s_shop_offer_6.xml',
+        # 'views/advance/snippet/s_banner/s_banner_17.xml',
+        # 'views/advance/snippet/s_shop_offer/s_shop_offer_7.xml',
+        # 'views/advance/snippet/s_text/s_text.xml',
+        # 'views/advance/snippet/s_footer/s_footer.xml',
+        # 'views/advance/snippet/s_header/s_header.xml',
 
         #Snippet Conedera
-        #'views/conedera/snippet/s_footer/footers.xml',
-        #'views/conedera/product_detail_page.xml',
-        #'views/conedera/snippets_register_conedera.xml',
-        #'views/conedera/headers.xml',
-        #'views/conedera/website_template.xml',
+        'views/conedera/snippet/s_footer/footers.xml',
+        'views/conedera/snippet/s_cover/s_cover_1.xml',
+        'views/conedera/snippet/s_d_2_column/dynamic_snippets.xml',
+        'views/conedera/product_detail_page.xml',
+        'views/conedera/snippets_register_conedera.xml',
+        'views/conedera/headers.xml',
+        'views/conedera/snippet/s_banner/s_banner_16.xml,'
+        'views/conedera/website_template.xml',
 
     ],
     'assets': {
