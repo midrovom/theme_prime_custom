a6056aef7e6c28efcb3904295f63489431e7ccbd
Diego Arcos oficce <darcos@callphoneecuador.com>
2026-01-05 12:21:56 -0500

    fix-update: ...

--- theme_prime_custom/__manifest__.py
+++ theme_prime_custom/__manifest__.py
@@ -21,9 +21,9 @@
         'views/conedera/snippet/s_cover/s_cover_1.xml',
         'views/conedera/snippet/s_d_2_column/dynamic_snippets.xml',
         'views/conedera/product_detail_page.xml',
-        'views/conedera/snippets_register_conedera.xml',
         'views/conedera/snippet/s_header/headers.xml',
-        'views/conedera/snippet/s_banner/s_banner_16.xml,'
+        'views/conedera/snippet/s_banner/s_banner_16.xml',
+        'views/conedera/snippets_register_conedera.xml',
         'views/conedera/website_template.xml',
 
     ],
