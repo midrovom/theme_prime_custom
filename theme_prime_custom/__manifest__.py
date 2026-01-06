b1736c5e7dbeede37e1d4c5477e469deadf6b539
Diego Arcos oficce <darcos@callphoneecuador.com>
2026-01-06 12:47:16 -0500

    fix-update: ...

--- theme_prime_custom/__manifest__.py
+++ theme_prime_custom/__manifest__.py
@@ -6,26 +6,31 @@
     'depends': ['website', 'theme_prime', 'conedera_theme_common'],
     'data': [
         # Snippet Advance
-        # 'views/advance/snippets_register_advance.xml',
-        # 'views/advance/snippet/s_banner/s_banner_9_custom.xml',
-        # 'views/advance/snippet/s_cover/s_cover_6.xml',
-        # 'views/advance/snippet/s_shop_offer/s_shop_offer_6.xml',
-        # 'views/advance/snippet/s_banner/s_banner_17.xml',
-        # 'views/advance/snippet/s_shop_offer/s_shop_offer_7.xml',
-        # 'views/advance/snippet/s_text/s_text.xml',
+        'views/advance/snippets_register_advance.xml',
+        'views/advance/snippet/s_banner/s_banner_9_custom.xml',
+        'views/advance/snippet/s_cover/s_cover_6.xml',
+        'views/advance/snippet/s_shop_offer/s_shop_offer_6.xml',
+        'views/advance/snippet/s_banner/s_banner_17.xml',
+        'views/advance/snippet/s_shop_offer/s_shop_offer_7.xml',
+        'views/advance/snippet/s_text/s_text.xml',
+
         'views/advance/snippet/s_footer/s_footer.xml',
         'views/advance/snippet/s_header/s_header.xml',
 
         #Snippet Conedera
-        'views/conedera/snippet/s_footer/footers.xml',
+
         'views/conedera/snippet/s_cover/s_cover_1.xml',
         'views/conedera/snippet/s_d_2_column/dynamic_snippets.xml',
         'views/conedera/product_detail_page.xml',
         'views/conedera/snippet/s_banner/s_banner_16.xml',
         'views/conedera/snippets_register_conedera.xml',
+
+        'views/conedera/snippet/s_footer/footers.xml',
         'views/conedera/snippet/s_header/headers.xml',
+
         'views/conedera/website_template.xml',
 
+        #version mobil advance / conedera
         'views/header_mobil.xml',
     ],
     'assets': {
