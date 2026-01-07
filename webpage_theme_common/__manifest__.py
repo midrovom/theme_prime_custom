0183e7d1a2d55923d0d9bba99ea2e8e7dc6994a0
midrovom <midrovo@callphoneecuador.com>
2026-01-07 12:00:30 -0500

    actualizacion de rutas advance

--- webpage_theme_common/__manifest__.py
+++ webpage_theme_common/__manifest__.py
@@ -25,25 +25,25 @@
     'assets': {
         'web.assets_frontend': [
             #Advance
-            'webpage_theme_common/src/scss/advance/s_cover/s_cover_6_.scss',
-            'webpage_theme_common/src/scss/advance/gallery_custom/gallery_custom.scss',
-            'webpage_theme_common/src/scss/advance/s_text/s_text.scss',
-            'webpage_theme_common/src/scss/advance/s_banner/s_banner_16.scss',
-            'webpage_theme_common/src/scss/advance/s_banner/s_banner_17.scss',
+            'webpage_theme_common/static/src/scss/advance/s_cover/s_cover_6_.scss',
+            'webpage_theme_common/static/src/scss/advance/gallery_custom/gallery_custom.scss',
+            'webpage_theme_common/static/src/scss/advance/s_text/s_text.scss',
+            'webpage_theme_common/static/src/scss/advance/s_banner/s_banner_16.scss',
+            'webpage_theme_common/static/src/scss/advance/s_banner/s_banner_17.scss',
 
-            'webpage_theme_common/src/scss/advance/s_shop_offert/s_shop_offer_6.scss',
-            'webpage_theme_common/src/scss/advance/s_shop_offert/shop_offert_7.scss',
-            'webpage_theme_common/src/scss/advance/s_footer/s_footer.scss',
+            'webpage_theme_common/static/src/scss/advance/s_shop_offert/s_shop_offer_6.scss',
+            'webpage_theme_common/static/src/scss/advance/s_shop_offert/shop_offert_7.scss',
+            #'webpage_theme_common/static/src/scss/advance/s_footer/s_footer.scss',
 
-            'webpage_theme_common/src/scss/advance/dynamic_custom/dynamic_custom.scss',
-            'webpage_theme_common/src/scss/advance/dynamic_custom/dynamic_filter_template_product_public_category_style_1.scss',
-            'webpage_theme_common/src/scss/advance/snippets/dynamic_snippet_carousel_custom/dynamic_snippet_products_extend.js',
+            'webpage_theme_common/static/src/scss/advance/dynamic_custom/dynamic_custom.scss',
+            'webpage_theme_common/static/src/scss/advance/dynamic_custom/dynamic_filter_template_product_public_category_style_1.scss',
+            'webpage_theme_common/static/src/scss/advance/snippets/dynamic_snippet_carousel_custom/dynamic_snippet_products_extend.js',
 
-            'webpage_theme_common/src/scss/advance/dynamic_custom/product_snippet_template_custom.scss',
-            'webpage_theme_common/src/scss/advance/product_item.scss',
+            'webpage_theme_common/static/src/scss/advance/dynamic_custom/product_snippet_template_custom.scss',
+            'webpage_theme_common/static/src/scss/advance/product_item.scss',
     ],
         'website.assets_wysiwyg': [
-            'webpage_theme_common/src/scss/advance/snippets/s_dynamic_snippet_categories/option.js',
+            'webpage_theme_common/static/src/scss/advance/snippets/s_dynamic_snippet_categories/option.js',
         ],
     },
     'installable': True,
