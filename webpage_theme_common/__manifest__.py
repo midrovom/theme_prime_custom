6978c405c450445f84fe51f08f1ba3e2fb54d6eb
midrovom <midrovo@callphoneecuador.com>
2026-01-07 17:52:44 -0500

    actualizacion de ruta

--- webpage_theme_common/__manifest__.py
+++ webpage_theme_common/__manifest__.py
@@ -14,12 +14,12 @@
         # 'data/conedera/brand_snippet_template_data.xml',
 
         #Advance
-        'views/snippets/advance/product_public_categ_view.xml',
-        'views/snippets/advance/s_dynamic_snippet_categories_preview_data.xml',
-        'views/snippets/advance/s_dynamic_snippet_category.xml',
+        # 'views/snippets/advance/product_public_categ_view.xml',
+        # 'views/snippets/advance/s_dynamic_snippet_categories_preview_data.xml',
+        # 'views/snippets/advance/s_dynamic_snippet_category.xml',
 
-        'views/snippets/advance/snippets.xml',
-        'views/snippets/advance/product_attribute_view.xml',
+        # 'views/snippets/advance/snippets.xml',
+        # 'views/snippets/advance/product_attribute_view.xml',
 
         #Conedera
         # 'views/snippets/conedera/website_template.xml',
@@ -34,22 +34,22 @@
     'assets': {
         'web.assets_frontend': [
             #Advance
-            'webpage_theme_common/static/src/scss/advance/s_cover/s_cover_6_.scss',
-            'webpage_theme_common/static/src/scss/advance/gallery_custom/gallery_custom.scss',
-            'webpage_theme_common/static/src/scss/advance/s_text/s_text.scss',
-            'webpage_theme_common/static/src/scss/advance/s_banner/s_banner_16.scss',
-            'webpage_theme_common/static/src/scss/advance/s_banner/s_banner_17.scss',
+            # 'webpage_theme_common/static/src/scss/advance/s_cover/s_cover_6_.scss',
+            # 'webpage_theme_common/static/src/scss/advance/gallery_custom/gallery_custom.scss',
+            # 'webpage_theme_common/static/src/scss/advance/s_text/s_text.scss',
+            # 'webpage_theme_common/static/src/scss/advance/s_banner/s_banner_16.scss',
+            # 'webpage_theme_common/static/src/scss/advance/s_banner/s_banner_17.scss',
 
-            'webpage_theme_common/static/src/scss/advance/s_shop_offert/s_shop_offer_6.scss',
-            'webpage_theme_common/static/src/scss/advance/s_shop_offert/shop_offert_7.scss',
-            'webpage_theme_common/static/src/scss/advance/s_footer/s_footer.scss',
+            # 'webpage_theme_common/static/src/scss/advance/s_shop_offert/s_shop_offer_6.scss',
+            # 'webpage_theme_common/static/src/scss/advance/s_shop_offert/shop_offert_7.scss',
+            # 'webpage_theme_common/static/src/scss/advance/s_footer/s_footer.scss',
 
-            'webpage_theme_common/static/src/scss/advance/dynamic_custom/dynamic_custom.scss',
-            'webpage_theme_common/static/src/scss/advance/dynamic_custom/dynamic_filter_template_product_public_category_style_1.scss',
-            'webpage_theme_common/static/src/scss/advance/snippets/dynamic_snippet_carousel_custom/dynamic_snippet_products_extend.js',
+            # 'webpage_theme_common/static/src/scss/advance/dynamic_custom/dynamic_custom.scss',
+            # 'webpage_theme_common/static/src/scss/advance/dynamic_custom/dynamic_filter_template_product_public_category_style_1.scss',
+            # 'webpage_theme_common/static/src/scss/advance/snippets/dynamic_snippet_carousel_custom/dynamic_snippet_products_extend.js',
 
-            'webpage_theme_common/static/src/scss/advance/dynamic_custom/product_snippet_template_custom.scss',
-            'webpage_theme_common/static/src/scss/advance/product_item.scss',
+            # 'webpage_theme_common/static/src/scss/advance/dynamic_custom/product_snippet_template_custom.scss',
+            # 'webpage_theme_common/static/src/scss/advance/product_item.scss',
 
             #Conedera
             # 'webpage_theme_common/static/src/scss/conedera/website.scss',
