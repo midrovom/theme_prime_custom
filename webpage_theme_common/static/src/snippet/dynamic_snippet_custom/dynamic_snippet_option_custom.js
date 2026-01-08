ed03c20a7bdfa9ced1eae0cbb285232785bb8fd2
midrovom <midrovo@callphoneecuador.com>
2026-01-08 11:25:13 -0500

    actualizacion de rutas

---
+++ webpage_theme_common/static/src/snippet/dynamic_snippet_custom/dynamic_snippet_option_custom.js
@@ -0,0 +1,20 @@
+/** @odoo-module **/
+import publicWidget from "@web/legacy/js/public/public_widget";
+import DynamicSnippetProducts from "@website_sale/snippets/s_dynamic_snippet_products/000";
+
+const DynamicSnippetProductsWysiwyg = DynamicSnippetProducts.extend({
+    _getQWebRenderOptions() {
+        const options = this._super(...arguments);
+        const tplKey = this.$el.data("template-key");
+
+        // 🔑 Lógica especial para el builder
+        if (this.editableMode && tplKey && tplKey.includes("dynamic_filter_template_product_product_style_2")) {
+            options.chunkSize = 1; // siempre 1 producto en builder
+        }
+
+        return options;
+    },
+});
+
+publicWidget.registry.dynamic_snippet_products = DynamicSnippetProductsWysiwyg;
+export default DynamicSnippetProductsWysiwyg;
