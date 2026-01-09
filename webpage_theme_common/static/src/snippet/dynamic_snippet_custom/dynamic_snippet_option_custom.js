8ceab3faf527c217ecdca0ab145fe86e1626bd98
midrovom <midrovo@callphoneecuador.com>
2026-01-09 09:33:42 -0500

    actualizacion de rutas

--- webpage_theme_common/static/src/snippet/dynamic_snippet_custom/dynamic_snippet_option_custom.js
+++ webpage_theme_common/static/src/snippet/dynamic_snippet_custom/dynamic_snippet_option_custom.js
@@ -1,20 +1,37 @@
 /** @odoo-module **/
-import publicWidget from "@web/legacy/js/public/public_widget";
-import DynamicSnippetProducts from "@website_sale/snippets/s_dynamic_snippet_products/000";
+import options from "@web_editor/js/editor/snippets.options";
 
-const DynamicSnippetProductsWysiwyg = DynamicSnippetProducts.extend({
+options.registry.dynamic_snippet_products.include({
     _getQWebRenderOptions() {
         const options = this._super(...arguments);
-        const tplKey = this.$el.data("template-key");
+        const tplKey = this.$target.data("template-key");
 
-        // 🔑 Lógica especial para el builder
         if (this.editableMode && tplKey && tplKey.includes("dynamic_filter_template_product_product_style_2")) {
-            options.chunkSize = 1; // siempre 1 producto en builder
+            options.chunkSize = 1; // builder → siempre 1 producto
         }
-
         return options;
     },
 });
 
-publicWidget.registry.dynamic_snippet_products = DynamicSnippetProductsWysiwyg;
-export default DynamicSnippetProductsWysiwyg;
+
+
+// /** @odoo-module **/
+// import publicWidget from "@web/legacy/js/public/public_widget";
+// import DynamicSnippetProducts from "@website_sale/snippets/s_dynamic_snippet_products/000";
+
+// const DynamicSnippetProductsWysiwyg = DynamicSnippetProducts.extend({
+//     _getQWebRenderOptions() {
+//         const options = this._super(...arguments);
+//         const tplKey = this.$el.data("template-key");
+
+//         // 🔑 Lógica especial para el builder
+//         if (this.editableMode && tplKey && tplKey.includes("dynamic_filter_template_product_product_style_2")) {
+//             options.chunkSize = 1; // siempre 1 producto en builder
+//         }
+
+//         return options;
+//     },
+// });
+
+// publicWidget.registry.dynamic_snippet_products = DynamicSnippetProductsWysiwyg;
+// export default DynamicSnippetProductsWysiwyg;
