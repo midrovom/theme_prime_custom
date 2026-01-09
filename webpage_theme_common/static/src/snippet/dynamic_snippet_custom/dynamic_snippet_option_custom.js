c04dfaf939e3a09af1fb3876872da34c590f39f4
midrovom <midrovo@callphoneecuador.com>
2026-01-09 10:01:51 -0500

    actualizacion de rutas

--- webpage_theme_common/static/src/snippet/dynamic_snippet_custom/dynamic_snippet_option_custom.js
+++ webpage_theme_common/static/src/snippet/dynamic_snippet_custom/dynamic_snippet_option_custom.js
@@ -1,38 +1,14 @@
-
 /** @odoo-module **/
 import options from "@web_editor/js/editor/snippets.options";
-import { utils as uiUtils } from "@web/core/ui/ui_service";
 
 options.registry.dynamic_snippet_products.include({
     _getQWebRenderOptions() {
         const options = this._super(...arguments);
         const tplKey = this.$target.data("template-key");
 
-        //  Builder → siempre 1 producto en estilo 2, incluso con datos ficticios
         if (this.editableMode && tplKey && tplKey.includes("dynamic_filter_template_product_product_style_2")) {
-            options.chunkSize = 1;
+            options.chunkSize = 1; // builder → siempre 1 producto
         }
-
-        // Frontend móvil → también 1 producto por slide
-        if (uiUtils.isSmall()) {
-            options.chunkSize = 1;
-        }
-
         return options;
     },
 });
-
-// /** @odoo-module **/
-// import options from "@web_editor/js/editor/snippets.options";
-
-// options.registry.dynamic_snippet_products.include({
-//     _getQWebRenderOptions() {
-//         const options = this._super(...arguments);
-//         const tplKey = this.$target.data("template-key");
-
-//         if (this.editableMode && tplKey && tplKey.includes("dynamic_filter_template_product_product_style_2")) {
-//             options.chunkSize = 1; // builder → siempre 1 producto
-//         }
-//         return options;
-//     },
-// });
