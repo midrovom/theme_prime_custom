4bf7b684655779d00aae7a94824ca3cfc1bccfbe
midrovom <midrovo@callphoneecuador.com>
2026-01-12 09:19:12 -0500

    actualizacion de rutas

--- theme_prime_custom/controllers/main.py
+++ theme_prime_custom/controllers/main.py
@@ -3,6 +3,7 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 from odoo.http import request
 from odoo.tools import formatLang
 from odoo import http
+
 class ThemePrimeMainClassExtended(ThemePrimeMainClass):
 
     def _prepare_product_data(self, products, fields, pricelist, options=None):
@@ -10,29 +11,60 @@ class ThemePrimeMainClassExtended(ThemePrimeMainClass):
         fields += ['attribute_line_ids']
         result = super()._prepare_product_data(products, fields, pricelist, options)
 
-        # Solo aplicar si la compañía del sitio web es la 1
-        if request.website.company_id.id == 1:
-            for res_product, product in zip(result, products):
-                res_product['attributes'] = []
-                res_product['other_attributes'] = []
+        # Aplicar siempre, sin importar la compañía
+        for res_product, product in zip(result, products):
+            res_product['attributes'] = []
+            res_product['other_attributes'] = []
 
-                for line in product.attribute_line_ids:
-                    if line.attribute_id.dr_is_brand:
-                        continue
+            for line in product.attribute_line_ids:
+                # Saltar si es marca
+                if line.attribute_id.dr_is_brand:
+                    continue
 
-                    for val in line.value_ids:
-                        attr_data = {
-                            'id': val.id,
-                            'name': val.name,
-                            'image': val.dr_image and f'/web/image/product.attribute.value/{val.id}/dr_image' or False,
-                            'attribute_name': line.attribute_id.name,
-                        }
-                        if line.attribute_id.attribute_custom:
-                            res_product['attributes'].append(attr_data)
-                        else:
-                            res_product['other_attributes'].append(attr_data)
+                for val in line.value_ids:
+                    attr_data = {
+                        'id': val.id,
+                        'name': val.name,
+                        'image': val.dr_image and f'/web/image/product.attribute.value/{val.id}/dr_image' or False,
+                        'attribute_name': line.attribute_id.name,
+                    }
+                    if line.attribute_id.attribute_custom:
+                        res_product['attributes'].append(attr_data)
+                    else:
+                        res_product['other_attributes'].append(attr_data)
 
         return result
+
+# class ThemePrimeMainClassExtended(ThemePrimeMainClass):
+
+#     def _prepare_product_data(self, products, fields, pricelist, options=None):
+#         fields = list(set(fields or []))
+#         fields += ['attribute_line_ids']
+#         result = super()._prepare_product_data(products, fields, pricelist, options)
+
+#         # Solo aplicar si la compañía del sitio web es la 1
+#         if request.website.company_id.id == 1:
+#             for res_product, product in zip(result, products):
+#                 res_product['attributes'] = []
+#                 res_product['other_attributes'] = []
+
+#                 for line in product.attribute_line_ids:
+#                     if line.attribute_id.dr_is_brand:
+#                         continue
+
+#                     for val in line.value_ids:
+#                         attr_data = {
+#                             'id': val.id,
+#                             'name': val.name,
+#                             'image': val.dr_image and f'/web/image/product.attribute.value/{val.id}/dr_image' or False,
+#                             'attribute_name': line.attribute_id.name,
+#                         }
+#                         if line.attribute_id.attribute_custom:
+#                             res_product['attributes'].append(attr_data)
+#                         else:
+#                             res_product['other_attributes'].append(attr_data)
+
+#         return result
 
     # Función para mostrar precio tachado
     def _get_computed_product_price(self, product, product_data, price_public_visibility, visibility_label, currency_id):
