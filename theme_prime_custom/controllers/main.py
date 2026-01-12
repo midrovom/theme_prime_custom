f3b646b3386345352dfcace5b7d158cae8bd9ee3
midrovom <midrovo@callphoneecuador.com>
2026-01-12 14:23:26 -0500

    actualizacion de controlador

--- theme_prime_custom/controllers/main.py
+++ theme_prime_custom/controllers/main.py
@@ -4,37 +4,6 @@ from odoo.http import request
 from odoo.tools import formatLang
 from odoo import http
 
-# class ThemePrimeMainClassExtended(ThemePrimeMainClass):
-
-#     def _prepare_product_data(self, products, fields, pricelist, options=None):
-#         fields = list(set(fields or []))
-#         fields += ['attribute_line_ids']
-#         result = super()._prepare_product_data(products, fields, pricelist, options)
-
-#         # Aplicar siempre, sin importar la compañía
-#         for res_product, product in zip(result, products):
-#             res_product['attributes'] = []
-#             res_product['other_attributes'] = []
-
-#             for line in product.attribute_line_ids:
-#                 # Saltar si es marca
-#                 if line.attribute_id.dr_is_brand:
-#                     continue
-
-#                 for val in line.value_ids:
-#                     attr_data = {
-#                         'id': val.id,
-#                         'name': val.name,
-#                         'image': val.dr_image and f'/web/image/product.attribute.value/{val.id}/dr_image' or False,
-#                         'attribute_name': line.attribute_id.name,
-#                     }
-#                     if line.attribute_id.attribute_custom:
-#                         res_product['attributes'].append(attr_data)
-#                     else:
-#                         res_product['other_attributes'].append(attr_data)
-
-#         return result
-
 class ThemePrimeMainClassExtended(ThemePrimeMainClass):
 
     def _prepare_product_data(self, products, fields, pricelist, options=None):
