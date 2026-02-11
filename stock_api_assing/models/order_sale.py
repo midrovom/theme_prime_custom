04fd17a86388a6cf1fadb0fe3f3c2b14d967ffc0
midrovom <midrovo@callphoneecuador.com>
2026-02-11 09:04:17 -0500

    nuevo campo

--- stock_api_assing/models/order_sale.py
+++ stock_api_assing/models/order_sale.py
@@ -174,15 +174,6 @@ class CustomSaleOrder(models.Model):
 
     @api.depends_context('lang')
     @api.depends('order_line.tax_id', 'order_line.negotiable_price', 'amount_total', 'amount_untaxed', 'currency_id')
-    # def _compute_tax_totals_negotiable(self):
-    #     for order in self:
-    #         order = order.with_company(order.company_id)
-    #         order_lines = order.order_line.filtered(lambda x: not x.display_type)
-    #         order.tax_totals_negotiable = order.env['account.tax']._prepare_tax_totals(
-    #             [x._convert_to_tax_base_line_dict_negotiable() for x in order_lines],
-    #             order.currency_id or order.company_id.currency_id,
-    #         )
-
     def _compute_tax_totals_negotiable(self):
         AccountTax = self.env['account.tax']
         for order in self:
@@ -197,6 +188,15 @@ class CustomSaleOrder(models.Model):
                 currency=order.currency_id or order.company_id.currency_id,
                 company=order.company_id,
             )
+
+    # def _compute_tax_totals_negotiable(self):
+    #     for order in self:
+    #         order = order.with_company(order.company_id)
+    #         order_lines = order.order_line.filtered(lambda x: not x.display_type)
+    #         order.tax_totals_negotiable = order.env['account.tax']._prepare_tax_totals(
+    #             [x._convert_to_tax_base_line_dict_negotiable() for x in order_lines],
+    #             order.currency_id or order.company_id.currency_id,
+    #         )
 
     def selection_multi(self):
         warehouse_id = self.env.context.get('warehouse_id')
