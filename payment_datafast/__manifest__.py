e5a3855412469b84fe2e8a93d94b3ed6ef8315bc
midrovom <midrovo@callphoneecuador.com>
2026-02-24 17:34:32 -0500

    se agrega el modulo de datafast

--- payment_datafast/__manifest__.py
+++ payment_datafast/__manifest__.py
@@ -32,11 +32,15 @@
         'data/payment_provider_data.xml', 
     ],
     'assets': {
-        'web.assets_frontend': [
+        'web.assets_frontend': {
+            'append': [
+                
             'payment_datafast/static/src/js/payment_form.js',
             'payment_datafast/static/src/js/wpwl_options.js',
             'payment_datafast/static/src/css/style_datafast.css',
-        ],
+
+            ],
+        },
     },
     'application': False,
     'installable': True,
