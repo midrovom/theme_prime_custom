a833ec92923ba70584e3c09a6f2b58227862bb3a
midrovom <midrovo@callphoneecuador.com>
2026-02-25 18:06:42 -0500

    actualizacion de js

--- payment_datafast/static/src/js/payment_form.js
+++ payment_datafast/static/src/js/payment_form.js
@@ -1,19 +1,15 @@
 /** @odoo-module **/
 
-import checkoutForm from 'payment.checkout_form';
-import manageForm from 'payment.manage_form';
+import publicWidget from '@web/legacy/js/public/public_widget';
 
-const datafastMixin = {
+publicWidget.registry.PaymentForm = publicWidget.registry.PaymentForm.extend({
     _processRedirectFlow(providerCode, paymentOptionId, paymentMethodCode, processingValues) {
         if (providerCode !== "datafast") {
             return this._super(...arguments);
         }
         window.location.href = processingValues.redirect_form_html;
     },
-};
-
-checkoutForm.include(datafastMixin);
-manageForm.include(datafastMixin);
+});
 
 
 // odoo.define("payment_datafast.payment_form", require => {
