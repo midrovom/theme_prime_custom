ec35b79b79a23e8108b2415ef72b916ccd1cc0b0
midrovom <midrovo@callphoneecuador.com>
2026-02-25 15:32:25 -0500

    nuevo cambio

--- payment_datafast/static/src/js/payment_form.js
+++ payment_datafast/static/src/js/payment_form.js
@@ -1,19 +1,39 @@
-odoo.define("payment_datafast.payment_form", require => {
-    'use strict';
+/** @odoo-module **/
 
-    const checkoutForm = require('payment.checkout_form');
-    const manageForm = require('payment.manage_form');
+import { patch } from "@web/core/utils/patch";
+import { CheckoutForm } from "@payment/js/checkout_form";
+import { ManageForm } from "@payment/js/manage_form";
 
-    const datafastMixin = {
-        _processRedirectPayment: function (code, providerId, processingValues) {
-            if (code !== "datafast") {
-                return this._super(...arguments);
-            }
-            window.location.href = processingValues.redirect_form_html;
-        },
+const datafastMixin = {
+    _processRedirectPayment(code, providerId, processingValues) {
+        if (code !== "datafast") {
+            return super._processRedirectPayment(...arguments);
+        }
 
-    }
+        window.location.href = processingValues.redirect_form_html;
+    },
+};
 
-    checkoutForm.include(datafastMixin);
-    manageForm.include(datafastMixin);
-});
+patch(CheckoutForm.prototype, "payment_datafast_checkout_patch", datafastMixin);
+patch(ManageForm.prototype, "payment_datafast_manage_patch", datafastMixin);
+
+
+// odoo.define("payment_datafast.payment_form", require => {
+//     'use strict';
+
+//     const checkoutForm = require('payment.checkout_form');
+//     const manageForm = require('payment.manage_form');
+
+//     const datafastMixin = {
+//         _processRedirectPayment: function (code, providerId, processingValues) {
+//             if (code !== "datafast") {
+//                 return this._super(...arguments);
+//             }
+//             window.location.href = processingValues.redirect_form_html;
+//         },
+
+//     }
+
+//     checkoutForm.include(datafastMixin);
+//     manageForm.include(datafastMixin);
+// });
