60e41e3b614c3a7558663bdc47dd7c411fbfbf3d
midrovom <midrovo@callphoneecuador.com>
2026-02-25 17:27:38 -0500

    nuevo cambio

--- payment_datafast/static/src/js/payment_form.js
+++ payment_datafast/static/src/js/payment_form.js
@@ -1,24 +1,3 @@
-// /** @odoo-module **/
-
-// import { patch } from "@web/core/utils/patch";
-// import { registry } from "@web/core/registry";
-
-// const serviceRegistry = registry.category("services");
-
-// if (registry.category("public_components").contains("payment.checkout_form")) {
-
-//     const CheckoutForm = registry.category("public_components").get("payment.checkout_form");
-
-//     patch(CheckoutForm.prototype, {
-//         _processRedirectPayment(code, providerId, processingValues) {
-//             if (code !== "datafast") {
-//                 return super._processRedirectPayment(...arguments);
-//             }
-//             window.location.href = processingValues.redirect_form_html;
-//         },
-//     });
-// }
-
 odoo.define("payment_datafast.payment_form", require => {
     'use strict';
 
@@ -26,15 +5,35 @@ odoo.define("payment_datafast.payment_form", require => {
     const manageForm = require('payment.manage_form');
 
     const datafastMixin = {
-        _processRedirectPayment: function (code, providerId, processingValues) {
-            if (code !== "datafast") {
+        _processRedirectFlow: function (providerCode, paymentOptionId, paymentMethodCode, processingValues) {
+            if (providerCode !== "datafast") {
                 return this._super(...arguments);
             }
             window.location.href = processingValues.redirect_form_html;
         },
-
-    }
+    };
 
     checkoutForm.include(datafastMixin);
     manageForm.include(datafastMixin);
 });
+
+
+// odoo.define("payment_datafast.payment_form", require => {
+//     'use strict';
+
+//     const checkoutForm = require('payment.checkout_form');
+//     const manageForm = require('payment.manage_form');
+
+//     const datafastMixin = {
+//         _processRedirectPayment: function (code, providerId, processingValues) {
+//             if (code !== "datafast") {
+//                 return this._super(...arguments);
+//             }
+//             window.location.href = processingValues.redirect_form_html;
+//         },
+
+//     }
+
+//     checkoutForm.include(datafastMixin);
+//     manageForm.include(datafastMixin);
+// });
