572a03c61bc355553991aa22597e8059b0cbbc7b
midrovom <midrovo@callphoneecuador.com>
2026-02-25 13:02:47 -0500

    nuevo cambio

--- payment_datafast/__init__.py
+++ payment_datafast/__init__.py
@@ -4,16 +4,16 @@ from . import wizard
 
 from odoo.addons.payment import setup_provider, reset_payment_provider
 
-def post_init_hook(env):
-    setup_provider(env, 'datafast')
+# def post_init_hook(env):
+#     setup_provider(env, 'datafast')
 
-def uninstall_hook(env):
-    reset_payment_provider(env, 'datafast')
-
-
-# def post_init_hook(cr, registry):
-#     setup_provider(cr, registry, 'datafast')
+# def uninstall_hook(env):
+#     reset_payment_provider(env, 'datafast')
 
 
-# def uninstall_hook(cr, registry):
-#     reset_payment_provider(cr, registry, 'datafast')
+def post_init_hook(cr, registry):
+    setup_provider(cr, registry, 'datafast')
+
+
+def uninstall_hook(cr, registry):
+    reset_payment_provider(cr, registry, 'datafast')
