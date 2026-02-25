6ec2f74d3dfaffcb90a0e8102f3b0b75b256ffa7
midrovom <midrovo@callphoneecuador.com>
2026-02-25 15:45:01 -0500

    nuevo cambio

--- payment_datafast/static/src/js/payment_form.js
+++ payment_datafast/static/src/js/payment_form.js
@@ -1,22 +1,25 @@
 /** @odoo-module **/
 
-odoo.define('payment_datafast.payment_form', function (require) {
-    'use strict';
+import { patch } from "@web/core/utils/patch";
+import { CheckoutForm } from "@payment/js/checkout/checkout_form";
+import { ManageForm } from "@payment/js/manage/manage_form";
 
-    const checkoutForm = require('payment.checkout_form');
-    const manageForm = require('payment.manage_form');
+patch(CheckoutForm.prototype, {
+    _processRedirectPayment(code, providerId, processingValues) {
+        if (code !== "datafast") {
+            return super._processRedirectPayment(...arguments);
+        }
+        window.location.href = processingValues.redirect_form_html;
+    },
+});
 
-    const datafastMixin = {
-        _processRedirectPayment: function (code, providerId, processingValues) {
-            if (code !== "datafast") {
-                return this._super(...arguments);
-            }
-            window.location.href = processingValues.redirect_form_html;
-        },
-    };
-
-    checkoutForm.include(datafastMixin);
-    manageForm.include(datafastMixin);
+patch(ManageForm.prototype, {
+    _processRedirectPayment(code, providerId, processingValues) {
+        if (code !== "datafast") {
+            return super._processRedirectPayment(...arguments);
+        }
+        window.location.href = processingValues.redirect_form_html;
+    },
 });
 
 // odoo.define("payment_datafast.payment_form", require => {
