cf5de3cb65f6dbcc3cf249dcf5f7e8881f2c2bf0
midrovom <midrovo@callphoneecuador.com>
2026-03-25 14:54:45 -0500

    nuevo

--- maintenance_report/models/maintenance_acta_entrega.py
+++ maintenance_report/models/maintenance_acta_entrega.py
@@ -4,3 +4,11 @@ class MaintenanceEquipment(models.Model):
     _inherit = 'maintenance.equipment'
 
     entregado_por_id = fields.Many2one('hr.employee', string='Responsable de Entrega', tracking=True)
+
+
+class Department(models.Model):
+    _inherit = "hr.department"
+
+    responsable_entrega_equipo = fields.Boolean( string="Responsable de entrega de equipo",
+        help="Marcar si este departamento es responsable de la entrega de equipos."
+    )
