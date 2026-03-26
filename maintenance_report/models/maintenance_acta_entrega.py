f6c53d8daef2727df5f44b1706dd186c4ddf75b0
midrovom <midrovo@callphoneecuador.com>
2026-03-26 09:41:04 -0500

    nuevo

--- maintenance_report/models/maintenance_acta_entrega.py
+++ maintenance_report/models/maintenance_acta_entrega.py
@@ -4,8 +4,6 @@ class MaintenanceEquipment(models.Model):
     _inherit = 'maintenance.equipment'
 
     entregado_por_id = fields.Many2one('hr.employee', string='Entrega RH', tracking=True)
-    enable_model_serial = fields.Boolean(string='Habilitar Modelo/Serial')
-
 
 class Department(models.Model):
     _inherit = "hr.department"
