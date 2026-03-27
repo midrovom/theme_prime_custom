0fd9126e0d35a4402545c271a35ff646f2197a14
midrovom <midrovo@callphoneecuador.com>
2026-03-27 10:57:19 -0500

    nuevo

--- maintenance_report/__manifest__.py
+++ maintenance_report/__manifest__.py
@@ -11,7 +11,7 @@
     'author': 'Ing. Bolivar Rodriguez',
     'depends': ['base','maintenance', 'hr'],
     'data': [
-        'security/ir.model.access.csv',
+        #'security/ir.model.access.csv',
         'report/maintenance_report.xml',
         'report/maintenance_report_templates.xml',
         'report/maintenance_equipment_return.xml',
