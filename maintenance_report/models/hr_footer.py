5e6c5c20676dab571b4e28acca512cb5abe81ed1
midrovom <midrovo@callphoneecuador.com>
2026-03-27 10:27:39 -0500

    nuevo

--- maintenance_report/models/hr_footer.py
+++ maintenance_report/models/hr_footer.py
@@ -5,19 +5,3 @@ class HrFooter(models.Model):
     _description = 'Footer'
 
     descripcion = fields.Text(string="Descripción")
-
-
-class ReportEquipment(models.AbstractModel):
-    _name = 'report.maintenance_report.report_equipment'
-    _description = 'Reporte de entrega de equipo'
-
-    @api.model
-    def _get_report_values(self, docids, data=None):
-        docs = self.env['maintenance.equipment'].browse(docids)
-        footer = self.env['hr.footer'].search([], limit=1)  
-        return {
-            'doc_ids': docids,
-            'doc_model': 'maintenance.equipment',
-            'docs': docs,
-            'footer': footer,
-        }
