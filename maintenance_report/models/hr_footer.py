3f891c08a0b53758bb25d879cb492a818fd4c5bc
midrovom <midrovo@callphoneecuador.com>
2026-03-27 10:19:21 -0500

    nuevo

--- maintenance_report/models/hr_footer.py
+++ maintenance_report/models/hr_footer.py
@@ -1,7 +1,23 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class HrFooter(models.Model):
     _name = 'hr.footer'
     _description = 'Footer'
 
     descripcion = fields.Text(string="Descripción")
+
+
+class ReportEquipment(models.AbstractModel):
+    _name = 'report.maintenance_report.report_equipment'
+    _description = 'Reporte de entrega de equipo'
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        docs = self.env['maintenance.equipment'].browse(docids)
+        footer = self.env['hr.footer'].search([], limit=1)  
+        return {
+            'doc_ids': docids,
+            'doc_model': 'maintenance.equipment',
+            'docs': docs,
+            'footer': footer,
+        }
