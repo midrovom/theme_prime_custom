e22a96f2c20e306c17ea646acb021c33d9e2e2e4
midrovom <midrovo@callphoneecuador.com>
2026-03-30 16:44:20 -0500

    se agrega modulo de mapa

--- webpage_theme_common/models/website_location.py
+++ webpage_theme_common/models/website_location.py
@@ -1,11 +1,21 @@
-from odoo import models, fields
+import re
+from odoo import models, fields, api
 
 class WebsiteLocation(models.Model):
     _name = 'website.location'
 
     name = fields.Char(string="Nombre del Local", required=True)
     city = fields.Char(string="Ciudad", required=True)
+    address = fields.Char(string="Dirección (Google Maps)", help="Dirección completa")
 
-    # Coordenadas
     latitude = fields.Char(string="Latitud")
     longitude = fields.Char(string="Longitud")
+
+    @api.onchange('address')
+    def _onchange_address(self):
+        if self.address:
+            # Buscar patrón @lat,lng en la URL
+            match = re.search(r'@(-?\d+\.\d+),(-?\d+\.\d+)', self.address)
+            if match:
+                self.latitude = match.group(1)
+                self.longitude = match.group(2)
