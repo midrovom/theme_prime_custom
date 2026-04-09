b14d2a4aca99259d5d0f188ef26de100de8aa95f
midrovom <midrovo@callphoneecuador.com>
2026-04-09 09:52:53 -0500

    nuevo

--- custom_web_hr_datos_candidatos/models/hr_footer.py
+++ custom_web_hr_datos_candidatos/models/hr_footer.py
@@ -1,11 +1,5 @@
 from odoo import models, fields
 
-class ResCompany(models.Model):
-    _inherit = "res.company"
-
-    footer = fields.Text(string="Footer de Reporte")
-
-
 class HrFooter(models.Model):
     _name = 'hr.footer'
     _description = 'Footer'
