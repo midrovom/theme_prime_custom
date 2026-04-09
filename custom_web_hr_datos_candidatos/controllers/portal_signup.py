02a94d58854c60198a47299aeda0d7318fe221e8
midrovom <midrovo@callphoneecuador.com>
2026-04-09 11:55:06 -0500

    nuevo

--- custom_web_hr_datos_candidatos/controllers/portal_signup.py
+++ custom_web_hr_datos_candidatos/controllers/portal_signup.py
@@ -1,15 +1,10 @@
+
+import secrets
 from odoo import http, _
 from odoo.http import request
-from odoo.exceptions import UserError, AccessDenied
-from markupsafe import Markup
-import secrets
-import werkzeug
-import logging
-from werkzeug.urls import url_encode
+from odoo.exceptions import UserError
 
-_logger = logging.getLogger(__name__)
-
-class PortalSignupController(http.Controller):
+class AuthSignupHomeCustom(http.Controller):
 
     @http.route('/web/signup', type='http', auth='public', website=True, sitemap=False)
     def web_auth_signup(self, *args, **kw):
@@ -20,50 +15,34 @@ class PortalSignupController(http.Controller):
 
         if 'error' not in qcontext and request.httprequest.method == 'POST':
             try:
-                if not request.env['ir.http']._verify_request_recaptcha_token('signup'):
-                    raise UserError(_("Suspicious activity detected by Google reCaptcha."))
+                self.do_signup(qcontext)
 
-                # Crear usuario portal con código de verificación
-                user = request.env['res.users'].sudo().create({
-                    'name': qcontext.get('name'),
-                    'login': qcontext.get('login'),
-                    'email': qcontext.get('login'),
-                    'password': qcontext.get('password'),
-                    'groups_id': [(6, 0, [request.env.ref('base.group_portal').id])],
-                    'email_verification_code': secrets.token_hex(16),
-                    'email_verified': False,
-                })
+                # Buscar el usuario recién creado
+                user = request.env['res.users'].sudo().search([('login', '=', qcontext.get('login'))], limit=1)
+                if user and user.has_group('base.group_portal'):
+                    user.write({
+                        'email_verification_code': secrets.token_hex(16),
+                        'email_verified': False,
+                    })
+                    template = request.env.ref('custom_web_hr_datos_candidatos.email_verification_template', raise_if_not_found=False)
+                    if template:
+                        template.sudo().send_mail(user.id, force_send=True)
+                    return request.render('custom_web_hr_datos_candidatos.signup_pending')
 
-                # Enviar correo con enlace de verificación
-                template = request.env.ref('mi_modulo_verificacion_correo.email_verification_template', raise_if_not_found=False)
-                if template:
-                    template.sudo().send_mail(user.id, force_send=True)
-
-                # Mostrar página de pendiente de verificación
-                return request.render('mi_modulo_verificacion_correo.signup_pending')
+                return self.web_login(*args, **kw)
 
             except UserError as e:
                 qcontext['error'] = e.args[0]
-            except Exception as e:
-                if request.env["res.users"].sudo().search_count([("login", "=", qcontext.get("login"))], limit=1):
-                    qcontext["error"] = _("Another user is already registered using this email address.")
-                else:
-                    _logger.warning("%s", e)
-                    qcontext['error'] = _("Could not create a new account.") + Markup('<br/>') + str(e)
-
-        elif 'signup_email' in qcontext:
-            user = request.env['res.users'].sudo().search([('email', '=', qcontext.get('signup_email')), ('state', '!=', 'new')], limit=1)
-            if user:
-                return request.redirect('/web/login?%s' % url_encode({'login': user.login, 'redirect': '/web'}))
 
         response = request.render('auth_signup.signup', qcontext)
-        response.headers['X-Frame-Options'] = 'SAMEORIGIN'
-        response.headers['Content-Security-Policy'] = "frame-ancestors 'self'"
         return response
 
+class VerifyEmailController(http.Controller):
 
-
-def _check_credentials(self, password, user_agent_env):
-    super()._check_credentials(password, user_agent_env)
-    if self.has_group('base.group_portal') and not self.email_verified:
-        raise AccessDenied(_("Debe verificar su correo antes de iniciar sesión."))
+    @http.route('/web/verify_email', type='http', auth='public', website=True)
+    def verify_email(self, token=None, **kwargs):
+        user = request.env['res.users'].sudo().search([('email_verification_code','=',token)], limit=1)
+        if user and user.has_group('base.group_portal'):
+            user.sudo().write({'email_verified': True})
+            return request.render('custom_web_hr_datos_candidatos.signup_success')
+        return request.render('custom_web_hr_datos_candidatos.signup_invalid')
--- custom_web_hr_datos_candidatos/models/res_users.py
+++ custom_web_hr_datos_candidatos/models/res_users.py
@@ -1,7 +1,13 @@
 from odoo import models, fields
+from odoo.exceptions import AccessDenied
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
     email_verification_code = fields.Char()
     email_verified = fields.Boolean(default=False)
+
+    def _check_credentials(self, password, user_agent_env):
+        super()._check_credentials(password, user_agent_env)
+        if self.has_group('base.group_portal') and not self.email_verified:
+            raise AccessDenied(_("Debe verificar su correo antes de iniciar sesión."))
