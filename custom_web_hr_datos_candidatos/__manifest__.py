82c1abcfb281a6fd6e3d6188c1863ec52af70f2f
midrovom <midrovo@callphoneecuador.com>
2026-04-09 09:52:12 -0500

    nuevo

--- custom_web_hr_datos_candidatos/__manifest__.py
+++ custom_web_hr_datos_candidatos/__manifest__.py
@@ -33,7 +33,6 @@
         'views/views_form/recruitment_form_3.xml',
         'views/views_form/website_hr_recruitment.xml',
         'views/error_template/error_templates.xml',
-        'views/footer/res_company_view.xml',
         'views/footer/hr_footer_views.xml',
 
     ],
