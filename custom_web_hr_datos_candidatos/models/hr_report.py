3c0acefdd8965e911e280b998c925b0ccd6bea18
midrovom <midrovo@callphoneecuador.com>
2026-04-14 16:41:03 -0500

    nuevo

--- custom_web_hr_datos_candidatos/models/hr_report.py
+++ custom_web_hr_datos_candidatos/models/hr_report.py
@@ -6,33 +6,33 @@ from odoo import models
 class HrApplicant(models.Model):
     _inherit = 'hr.applicant'
 
-def action_print_zip(self):
-    zip_buffer = io.BytesIO()
+    def action_print_zip(self):
+        zip_buffer = io.BytesIO()
 
-    with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zip_file:
-        for applicant in self:
-            pdf_content, _ = self.env['ir.actions.report']._render_qweb_pdf(
-                'custom_web_hr_datos_candidatos.report_applicant_test',
-                res_ids=[applicant.id]
-            )
+        with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zip_file:
+            for applicant in self:
+                pdf_content, _ = self.env['ir.actions.report']._render_qweb_pdf(
+                    'custom_web_hr_datos_candidatos.report_applicant_test',
+                    res_ids=[applicant.id]
+                )
 
-            name = (applicant.partner_name or 'Candidato').replace('/', '').replace('\\', '')
-            cedula = applicant.cedula or ''
+                name = (applicant.partner_name or 'Candidato').replace('/', '').replace('\\', '')
+                cedula = applicant.cedula or ''
 
-            filename = f"{cedula}_{name}.pdf"
-            zip_file.writestr(filename, pdf_content)
+                filename = f"{cedula}_{name}.pdf"
+                zip_file.writestr(filename, pdf_content)
 
-    zip_buffer.seek(0)
+        zip_buffer.seek(0)
 
-    attachment = self.env['ir.attachment'].create({
-        'name': 'Candidatos.zip',
-        'type': 'binary',
-        'datas': base64.b64encode(zip_buffer.read()),
-        'mimetype': 'application/zip'
-    })
+        attachment = self.env['ir.attachment'].create({
+            'name': 'Candidatos.zip',
+            'type': 'binary',
+            'datas': base64.b64encode(zip_buffer.read()),
+            'mimetype': 'application/zip'
+        })
 
-    return {
-        'type': 'ir.actions.act_url',
-        'url': f'/web/content/{attachment.id}?download=true',
-        'target': 'self',
-    }
+        return {
+            'type': 'ir.actions.act_url',
+            'url': f'/web/content/{attachment.id}?download=true',
+            'target': 'self',
+        }
