e1e6142821f34fe5a8e5ba53e79000ecde4e64d8
midrovom <midrovo@callphoneecuador.com>
2026-05-07 15:56:05 -0500

    nuevo

--- theme_prime_custom/static/src/js/prueba.js
+++ theme_prime_custom/static/src/js/prueba.js
@@ -1,78 +1,21 @@
-odoo.define('theme_prime_custom.carousel_options', function (require) {
+odoo.define('theme_prime_custom.carousel_extend', function (require) {
     'use strict';
 
     const options = require('web_editor.snippets.options');
 
-    options.registry.tp_custom_carousel = options.Class.extend({
+    options.registry.carousel.include({
 
         /**
-         * Agregar Slide
+         * Override cuando se agrega slide
          */
-        async addSlide(previewMode, widgetValue, params) {
+        async addSlide() {
 
-            const $carousel = this.$target;
-            const $inner = $carousel.find('.carousel-inner');
-            const $indicators = $carousel.find('.carousel-indicators');
+            await this._super(...arguments);
 
-            const index = $inner.find('.carousel-item').length;
+            console.log('Slide agregado en Theme Prime');
 
-            const slide = `
-                <div class="carousel-item p-0"
-                     data-name="Slide">
-
-                    <div class="container-fluid px-0">
-
-                        <div class="row g-0 align-items-center"
-                             style="min-height:300px;">
-
-                            <div class="col-12 col-lg-6 p-4">
-
-                                <div class="jumbotron rounded border p-4">
-
-                                    <h3>
-                                        Nuevo Slide ${index + 1}
-                                    </h3>
-
-                                    <h2 class="display-4 fw-bold">
-                                        Título dinámico
-                                    </h2>
-
-                                    <p class="lead">
-                                        Este slide fue agregado dinámicamente.
-                                    </p>
-
-                                    <a href="/shop"
-                                       class="btn btn-primary mt-2">
-                                        Ver más
-                                    </a>
-
-                                </div>
-
-                            </div>
-
-                            <div class="col-12 col-lg-6 text-center">
-
-                                <img class="img-fluid"
-                                     style="max-height:300px; object-fit:contain;"
-                                     src="/web/image/theme_prime.s_cover_3_1"/>
-
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                </div>
-            `;
-
-            $inner.append(slide);
-
-            $indicators.append(`
-                <button type="button"
-                        data-bs-target="#${$carousel.attr('id')}"
-                        data-bs-slide-to="${index}">
-                </button>
-            `);
         },
+
     });
+
 });
