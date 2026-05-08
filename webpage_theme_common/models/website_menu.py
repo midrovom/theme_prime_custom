37258a51b5098ebf1d2988318fae8d6c7eb23e54
midrovom <midrovo@callphoneecuador.com>
2026-05-08 10:54:55 -0500

    nuevo

--- webpage_theme_common/models/website_menu.py
+++ webpage_theme_common/models/website_menu.py
@@ -13,3 +13,12 @@ class Website(models.Model):
         string="Texto del buscador",
         help="Texto dinámico que se mostrará en el placeholder del buscador."
     )
+
+    search_placeholder_effect = fields.Selection([
+        ('none', 'Sin efecto'),
+        ('up_down', 'Arriba-Abajo'),
+        ('down_up', 'Abajo-Arriba'),
+        ('fade', 'Desvanecer'),
+        ('bounce', 'Rebotar'),
+    ], string="Efecto del texto", default='none')
+
