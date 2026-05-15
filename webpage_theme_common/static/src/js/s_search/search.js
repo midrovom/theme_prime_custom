a4e58d1add0fa743e32732baf08ba89f47503e47
midrovom <midrovo@callphoneecuador.com>
2026-05-15 15:20:21 -0500

    nuevo

--- webpage_theme_common/static/src/js/s_search/search.js
+++ webpage_theme_common/static/src/js/s_search/search.js
@@ -2,33 +2,28 @@
 
 import publicWidget from '@web/legacy/js/public/public_widget';
 
-odoo.define('webpage_theme_common.search', function (require) {
-    "use strict";
-    const publicWidget = require('web.public.widget');
+publicWidget.registry.SearchPlaceholderEffect = publicWidget.Widget.extend({
+    selector: '.tp-search-input',
+    start: function () {
+        const input = this.$el[0];
+        const frases = [];
+        const spans = document.querySelectorAll('.tp-fake-placeholder');
 
-    publicWidget.registry.SearchPlaceholderEffect = publicWidget.Widget.extend({
-        selector: '.tp-search-input',
-        start: function () {
-            const input = this.$el[0];
-            const frases = [];
-            const spans = document.querySelectorAll('.tp-fake-placeholder');
+        spans.forEach(span => frases.push(span.textContent.trim()));
 
-            spans.forEach(span => frases.push(span.textContent.trim()));
+        if (!frases.length) return;
 
-            if (!frases.length) return;
+        let index = 0;
+        const duracion = parseInt(spans[0].style.getPropertyValue('--anim-duration')) * 1000 || 2000;
 
-            let index = 0;
-            const duracion = parseInt(spans[0].style.getPropertyValue('--anim-duration')) * 1000 || 2000;
+        const cambiarFrase = () => {
+            input.setAttribute('placeholder', frases[index]);
+            spans.forEach(span => span.classList.remove('active'));
+            spans[index].classList.add('active');
+            index = (index + 1) % frases.length;
+        };
 
-            const cambiarFrase = () => {
-                input.setAttribute('placeholder', frases[index]);
-                spans.forEach(span => span.classList.remove('active'));
-                spans[index].classList.add('active');
-                index = (index + 1) % frases.length;
-            };
-
-            cambiarFrase();
-            setInterval(cambiarFrase, duracion);
-        },
-    });
+        cambiarFrase();
+        setInterval(cambiarFrase, duracion);
+    },
 });
