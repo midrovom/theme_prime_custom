5259d7a9d67c80c5634717b9b4f62df7c5bf5863
midrovom <midrovo@callphoneecuador.com>
2026-05-15 17:08:26 -0500

    nuevo

--- webpage_theme_common/static/src/js/s_search/search.js
+++ webpage_theme_common/static/src/js/s_search/search.js
@@ -6,15 +6,14 @@ publicWidget.registry.search = publicWidget.Widget.extend({
     selector: '.tp-search-input',
     start: function () {
         const input = this.$el[0];
-        const frases = [];
         const spans = document.querySelectorAll('.tp-fake-placeholder');
-
-        spans.forEach(span => frases.push(span.textContent.trim()));
+        const frases = Array.from(spans).map(span => span.textContent.trim());
 
         if (!frases.length) return;
 
         let index = 0;
-        const duracion = parseInt(spans[0].style.getPropertyValue('--anim-duration')) * 1000 || 2000;
+        // Tomamos la duración desde el primer span
+        const duracion = parseFloat(spans[0].style.getPropertyValue('--anim-duration')) * 1000 || 2000;
 
         const cambiarFrase = () => {
             input.setAttribute('placeholder', frases[index]);
