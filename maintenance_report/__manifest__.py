6bfb9401ae5a452200ae4680e952592cf59ac497
midrovom <midrovo@callphoneecuador.com>
2026-05-26 09:04:43 -0500

    nuevo

--- maintenance_report/__manifest__.py
+++ maintenance_report/__manifest__.py
@@ -9,9 +9,9 @@
         Este módulo permite generar Acta de entrega de equipos.
     """,
     'author': 'Ing. Bolivar Rodriguez',
-    'depends': ['maintenance','hr', 'mail'],
+    'depends': ['maintenance','hr'],
     'data': [
-        'security/security.xml',
+        #'security/security.xml',
         # 'security/ir.model.access.csv',
 
         'report/maintenance_report.xml',
@@ -22,7 +22,7 @@
     ],
     'assets': {
         'web.assets_frontend': [
-            'maintenance_report/static/src/js/chatter_readonly.js',
+            #'maintenance_report/static/src/js/chatter_readonly.js',
         ],
     },
 
