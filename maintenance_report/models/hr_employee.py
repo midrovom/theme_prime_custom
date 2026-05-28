a08c4db0b58f5f17945f3e88d4b33937e061e0b0
midrovom <midrovo@callphoneecuador.com>
2026-05-28 11:11:01 -0500

    nuevo

--- maintenance_report/models/hr_employee.py
+++ maintenance_report/models/hr_employee.py
@@ -1,13 +1,17 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    document_ids = fields.One2many(
-        'hr.employee.document',  # modelo relacionado
-        'employee_id',           # campo inverso
-        string='Documentos'
-    )
+    document_ids = fields.One2many('hr.employee.document', 'employee_id', string='Documentos')
+    is_readonly_group = fields.Boolean(compute='_compute_is_readonly_group', store=False)
+
+    @api.depends_context('uid')
+    def _compute_is_readonly_group(self):
+        for rec in self:
+            rec.is_readonly_group = self.env.user.has_group(
+                'tu_modulo.group_attachment_hr_readonly_custom'
+            )
 
 class HrEmployeeDocument(models.Model):
     _name = 'hr.employee.document'
@@ -16,3 +20,4 @@ class HrEmployeeDocument(models.Model):
     name = fields.Char(string='Nombre del documento', required=True)
     file = fields.Binary(string='Archivo')
     employee_id = fields.Many2one('hr.employee', string='Empleado', ondelete='cascade')
+
