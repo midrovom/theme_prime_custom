124aa19995ef5626afb26fb3d1d36f41543e917d
midrovom <midrovo@callphoneecuador.com>
2026-05-28 16:01:34 -0500

    nuevo

---
+++ maintenance_report/report/maintenance_report.py
@@ -0,0 +1,31 @@
+from odoo import models
+
+
+class ReportEquipment(models.AbstractModel):
+    _name = 'report.maintenance_report.report_equipment'
+    _description = 'Reporte Acta Entrega'
+
+    def _get_report_values(self, docids, data=None):
+        docs = self.env['maintenance.equipment'].browse(docids)
+
+        return {
+            'doc_ids': docids,
+            'doc_model': 'maintenance.equipment',
+            'docs': docs,
+            'data': data or {},
+        }
+
+
+class ReportEquipmentReturn(models.AbstractModel):
+    _name = 'report.maintenance_report.report_equipment_return'
+    _description = 'Reporte Acta Devolucion'
+
+    def _get_report_values(self, docids, data=None):
+        docs = self.env['maintenance.equipment'].browse(docids)
+
+        return {
+            'doc_ids': docids,
+            'doc_model': 'maintenance.equipment',
+            'docs': docs,
+            'data': data or {},
+        }
