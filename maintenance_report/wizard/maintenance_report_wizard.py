cfcde88ee4b7970f83edcbfd9f2bb5cf97a20f18
midrovom <midrovo@callphoneecuador.com>
2026-05-28 16:39:57 -0500

    nuevo

--- maintenance_report/wizard/maintenance_report_wizard.py
+++ maintenance_report/wizard/maintenance_report_wizard.py
@@ -5,10 +5,7 @@ class MaintenanceReportWizard(models.TransientModel):
     _name = 'maintenance.report.wizard'
     _description = 'Wizard Acta Entrega/Devolucion'
 
-    entregado_por_id = fields.Many2one(
-        'hr.employee',
-        string='Encargado de entrega/recepción',
-        required=True,
+    entregado_por_id = fields.Many2one('hr.employee', string='Encargado de entrega/recepción', required=True,
         domain="[('department_id.responsable_entrega_equipo','=',True)]"
     )
 
@@ -19,15 +16,8 @@ class MaintenanceReportWizard(models.TransientModel):
 
     def action_generate_report(self):
 
-        active_ids = self.env.context.get('active_ids', [])
-
-        # compatibilidad cuando viene solo active_id
-        if not active_ids:
-            active_id = self.env.context.get('active_id')
-            if active_id:
-                active_ids = [active_id]
-
-        equipments = self.env['maintenance.equipment'].browse(active_ids)
+        equipment_ids = self.env.context.get('active_ids')
+        equipments = self.env['maintenance.equipment'].browse(equipment_ids)
 
         if self.report_type == 'delivery':
             report = self.env.ref(
@@ -39,7 +29,7 @@ class MaintenanceReportWizard(models.TransientModel):
             )
 
         return report.report_action(
-            equipments.ids,
+            equipments,
             data={
                 'entregado_por_name': self.entregado_por_id.name,
                 'entregado_por_ci': self.entregado_por_id.identification_id,
