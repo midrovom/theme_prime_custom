745756386d45e3860eacc2a697103afffc1f0c07
midrovom <midrovo@callphoneecuador.com>
2026-07-21 17:24:21 -0500

    nuevo

--- stock_update/models/stock_quant.py
+++ stock_update/models/stock_quant.py
@@ -1,21 +1,20 @@
-from odoo import models, api
+from odoo import models
 
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
     def write(self, vals):
         res = super().write(vals)
-        agotado_ribbon = self.env.ref('stock_update.ribbon_out_of_stock', raise_if_not_found=False)
+        # Usamos el ID directo (2) o búsqueda por nombre
+        agotado_ribbon = self.env['product.ribbon'].browse(2)
 
         for quant in self:
             product = quant.product_id.product_tmpl_id
             if quant.inventory_quantity_auto_apply <= 0:
-                # Stock agotado
                 product.allow_out_of_stock_order = False
                 if agotado_ribbon:
                     product.website_ribbon_id = agotado_ribbon.id
             else:
-                # Stock disponible nuevamente
                 product.allow_out_of_stock_order = True
-                product.website_ribbon_id = False  # opcional: limpiar la etiqueta
+                product.website_ribbon_id = False
         return res
