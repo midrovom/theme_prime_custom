682cf6f81b3edc054f0364832253f2369b4a0f44
midrovom <midrovo@callphoneecuador.com>
2026-07-21 16:12:09 -0500

    nuevo

--- stock_api_update/models/stock_quant.py
+++ stock_api_update/models/stock_quant.py
@@ -3,21 +3,16 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
-class ProductTemplateInherit(models.Model):
-    _inherit = "product.template"
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
 
-    def update_ribbon_status(self):
-        products = self.search([])  # todos los productos
-        for product in products:
+    @api.depends('qty_available')
+    def _compute_stock_flags(self):
+        agotado_ribbon = self.env.ref('tu_modulo.ribbon_out_of_stock', raise_if_not_found=False)
+        for product in self:
             if product.qty_available <= 0:
-                product.write({
-                    'allow_out_of_stock_order': False,
-                    'website_ribbon_id': self.env.ref('website_sale_stock.ribbon_out_of_stock').id
-                })
-                _logger.info(f"Producto {product.default_code} marcado como AGOTADO")
+                product.allow_out_of_stock_order = False
+                if agotado_ribbon:
+                    product.website_ribbon_id = agotado_ribbon.id
             else:
-                product.write({
-                    'allow_out_of_stock_order': True,
-                    'website_ribbon_id': False
-                })
-                _logger.info(f"Producto {product.default_code} marcado como DISPONIBLE")
+                product.website_ribbon_id = False
