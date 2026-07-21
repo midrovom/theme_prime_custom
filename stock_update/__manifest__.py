8ebc15c9a379f8899723aa658472580963e21ef3
midrovom <midrovo@callphoneecuador.com>
2026-07-21 16:46:06 -0500

    nuevo

---
+++ stock_update/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "stock_update",
+    "version": "18.0.1.0.0",
+    'summary': """
+        Actualiza automáticamente ribbon y flag de venta sin stocks""",
+
+    'description': """
+       Este módulo personalizado ajusta automáticamente los campos
+        `allow_out_of_stock_order` y `website_ribbon_id` en product.template
+        cuando el stock disponible llega a cero.
+    """,
+    'author': "Ing. Bolivar Rodriguez",
+    'website': "https://www.callphone.com.ec",
+    'images': ['static/description/banner.png'],
+    "category": "Website",
+    "depends": ['stock', 'website_sale', 'website_sale_stock'],
+    "license": "AGPL-3",
+    'data': [
+
+    ],
+    'license': 'AGPL-3',
+    'post_load': '',
+    "installable": True,
+    'auto_install': True,
+    'application': True,
+}
--- stock_update/models/stock_quant.py
+++ stock_update/models/stock_quant.py
@@ -1,18 +1,20 @@
 from odoo import models, api
-import logging
-
-_logger = logging.getLogger(__name__)
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     @api.depends('qty_available')
     def _compute_stock_flags(self):
-        agotado_ribbon = self.env.ref('stock_api_update.ribbon_out_of_stock', raise_if_not_found=False)
+        """Si el stock llega a cero:
+        - Desactiva el check 'allow_out_of_stock_order'
+        - Asigna la etiqueta 'Agotado' en website_ribbon_id
+        """
+        agotado_ribbon = self.env.ref('theme_prime_custom.ribbon_out_of_stock', raise_if_not_found=False)
         for product in self:
             if product.qty_available <= 0:
                 product.allow_out_of_stock_order = False
                 if agotado_ribbon:
                     product.website_ribbon_id = agotado_ribbon.id
             else:
+                # Opcional: limpiar la etiqueta si vuelve a tener stock
                 product.website_ribbon_id = False
