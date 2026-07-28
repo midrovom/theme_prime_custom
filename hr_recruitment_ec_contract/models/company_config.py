f2968b8567813a4f8cfb0d5a3773ac0548fc0155
midrovom <midrovo@callphoneecuador.com>
2026-07-28 12:21:43 -0500

    nuevo

--- hr_recruitment_ec_contract/models/company_config.py
+++ hr_recruitment_ec_contract/models/company_config.py
@@ -5,8 +5,4 @@ class HrEcOnboardingConfig(models.Model):
     _description = 'Configuración de Empresas'
 
     name = fields.Char(string='Nombre de la Empresa', required=True)
-    # ruc = fields.Char(string='RUC', size=13)
-    # direccion = fields.Char(string='Dirección')
-    # telefono = fields.Char(string='Teléfono')
-    # email = fields.Char(string='Correo Electrónico')
     activo = fields.Boolean(string='Activo', default=True)
