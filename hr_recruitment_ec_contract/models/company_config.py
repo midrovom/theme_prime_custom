61069e532ec38816e1269982339bb9d8920acd74
midrovom <midrovo@callphoneecuador.com>
2026-07-29 10:23:55 -0500

    nuevo

--- hr_recruitment_ec_contract/models/company_config.py
+++ hr_recruitment_ec_contract/models/company_config.py
@@ -6,3 +6,14 @@ class HrEcOnboardingConfig(models.Model):
 
     name = fields.Char(string='Nombre de la Empresa', required=True)
     activo = fields.Boolean(string='Activo', default=True)
+
+class HrEmployee(models.Model):
+    _inherit = "hr.employee"
+
+    company_config_id = fields.Many2one(
+        "company.config",
+        related="contract_id.company_config_id",
+        string="Empresa Configurada",
+        readonly=True,
+        store=True,
+    )
