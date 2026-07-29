bae14ad5ad76859a5da3a34a833b919ca2bc0e29
midrovom <midrovo@callphoneecuador.com>
2026-07-29 14:22:53 -0500

    nuevo

--- hr_recruitment_ec_contract/models/company_config.py
+++ hr_recruitment_ec_contract/models/company_config.py
@@ -6,6 +6,10 @@ class HrEcOnboardingConfig(models.Model):
 
     name = fields.Char(string='Nombre de la Empresa', required=True)
     activo = fields.Boolean(string='Activo', default=True)
+    ruc_empresa = fields.Char(string='RUC de la Empresa', required=True)
+    representante_legal = fields.Char(string='Nombre del Representante Legal')
+    ruc_representante = fields.Char(string='RUC del Representante Legal')
+    correo_representante = fields.Char(string='Correo del Representante Legal')
 
 class HrEmployee(models.Model):
     _inherit = "hr.employee"
@@ -41,13 +45,4 @@ class HrEmployee(models.Model):
                             automatic_onboarding_generation=True
                         ).action_generate_documents()
         return res
-
-
-class HrEcOnboardingConfiginherit(models.Model):
-
-    _inherit = 'company.config'
-
-    ruc_empresa = fields.Char(string='RUC de la Empresa', required=True)
-    representante_legal = fields.Char(string='Nombre del Representante Legal')
-    ruc_representante = fields.Char(string='RUC del Representante Legal')
-    correo_representante = fields.Char(string='Correo del Representante Legal')
+    
