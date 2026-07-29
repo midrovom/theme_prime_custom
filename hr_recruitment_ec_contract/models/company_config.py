115946030bcb80d851acd592f57995a57e3ff5a6
midrovom <midrovo@callphoneecuador.com>
2026-07-29 11:11:22 -0500

    nuevo

--- hr_recruitment_ec_contract/models/company_config.py
+++ hr_recruitment_ec_contract/models/company_config.py
@@ -14,3 +14,30 @@ class HrEmployee(models.Model):
         "company.config",
         string="Empresa Afiliada",
     )
+
+    def write(self, vals):
+            old_company_config = {
+                employee.id: employee.company_config_id.id
+                for employee in self
+            }
+
+            res = super().write(vals)
+
+            if "company_config_id" in vals:
+                for employee in self:
+                    old_value = old_company_config.get(employee.id)
+                    new_value = employee.company_config_id.id
+
+                    # Solo ejecutar si realmente cambió
+                    if old_value != new_value and new_value:
+                        packages = self.env[
+                            "hr.ec.onboarding.package"
+                        ].sudo().search([
+                            ("employee_id", "=", employee.id),
+                        ])
+
+                        if packages:
+                            packages.with_context(
+                                automatic_onboarding_generation=True
+                            ).action_generate_documents()
+            return res
