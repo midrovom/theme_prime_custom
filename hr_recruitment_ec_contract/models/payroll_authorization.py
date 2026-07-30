7af45bb9ba7069aae83b9b61ddc196a0787a2a5a
midrovom <midrovo@callphoneecuador.com>
2026-07-30 10:13:19 -0500

    nuevo

--- hr_recruitment_ec_contract/models/payroll_authorization.py
+++ hr_recruitment_ec_contract/models/payroll_authorization.py
@@ -35,6 +35,11 @@ class HrEcPayrollAuthorization(models.Model):
             readonly=True,
         )
 
+    request_date = fields.Date(string="Fecha de solicitud", required=True,
+        default=fields.Date.today
+    )
+
+
     applicant_id = fields.Many2one("hr.applicant", string="Candidato", ondelete="set null")
     package_id = fields.Many2one("hr.ec.onboarding.package", string="Paquete", ondelete="set null")
     template_id = fields.Many2one("hr.ec.document.template", string="Plantilla")
