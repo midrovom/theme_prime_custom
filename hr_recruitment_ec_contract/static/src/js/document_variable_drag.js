6db52c3c773305ebc50fc67a082e6a76b64f68ef
midrovom <midrovo@callphoneecuador.com>
2026-07-31 14:51:03 -0500

    nuevo

--- hr_recruitment_ec_contract/static/src/js/document_variable_drag.js
+++ hr_recruitment_ec_contract/static/src/js/document_variable_drag.js
@@ -1,23 +1,54 @@
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
-import { Component } from "@odoo/owl";
+import { Component, onWillStart } from "@odoo/owl";
+import { useService } from "@web/core/utils/hooks";
+
 
 class VariableDragWidget extends Component {
+
     setup() {
-        const records = this.props.record.data.variable_ids?.records || [];
-        this.variables = records.map(r => ({
-            name: r.data?.name,
-            expression: r.data?.expression,
-        }));
+
+        this.orm = useService("orm");
+        this.variables = [];
+
+        onWillStart(async () => {
+            const ids = this.props.record.data.variable_ids?.resIds || [];
+
+            if (ids.length) {
+                const records = await this.orm.searchRead(
+                    "hr.ec.document.variable",
+                    [
+                        ["id", "in", ids]
+                    ],
+                    [
+                        "name",
+                        "expression"
+                    ]
+                );
+
+                this.variables = records;
+            }
+
+        });
+
     }
 
+
+
     onDragStart(ev, variable) {
-        ev.dataTransfer.setData("text/plain", variable.expression);
+
+        ev.dataTransfer.setData(
+            "text/plain",
+            variable.expression
+        );
+
     }
 
 }
 
-VariableDragWidget.template = "hr_ec_document.VariableDragWidget";
-
-registry.category("fields").add("variable_drag", VariableDragWidget);
+VariableDragWidget.template = "hr_recruitment_ec_contract.VariableDragWidget";
+registry.category("fields").add(
+    "variable_drag",
+    VariableDragWidget
+);
