1f6200b66ca3f18e71577cf4d00d87f3e66a4bbd
midrovom <midrovo@callphoneecuador.com>
2026-08-06 16:48:02 -0500

    nuevo

--- hr_recruitment_ec_contract/models/hr_applicant.py
+++ hr_recruitment_ec_contract/models/hr_applicant.py
@@ -246,12 +246,6 @@ class HrApplicant(models.Model):
                 rec.readonly_fields = rec.process_finalized or rec.is_readonly_group
 
     def action_finalize_process(self):
-            self.ensure_one()
-            self.process_finalized = True
-            return {
-                'type': 'ir.actions.act_window',
-                'res_model': 'hr.applicant',
-                'view_mode': 'form',
-                'res_id': self.id,
-                'target': 'current',
-            }
+        self.ensure_one()
+        self.process_finalized = True
+        return True
