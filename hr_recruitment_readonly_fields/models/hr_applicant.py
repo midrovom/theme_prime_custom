31b4a5422e25f69436fbec629d3e85e64e18e0ea
midrovom <midrovo@callphoneecuador.com>
2026-08-06 17:34:10 -0500

    nuevo

--- hr_recruitment_readonly_fields/models/hr_applicant.py
+++ hr_recruitment_readonly_fields/models/hr_applicant.py
@@ -4,20 +4,19 @@ class HrApplicant(models.Model):
     _inherit = 'hr.applicant'
 
     process_finalized = fields.Boolean(string="Proceso Finalizado", default=False)
-
-    is_readonly_group = fields.Boolean(
-        compute='_compute_is_readonly_group',
-        store=False
-    )
+    is_readonly_finalize = fields.Boolean(compute='_compute_is_readonly_finalize', store=False)
 
     @api.depends_context('uid')
-    def _compute_is_readonly_group(self):
+    def _compute_is_readonly_finalize(self):
         for rec in self:
-            rec.is_readonly_group = self.env.user.has_group(
+            rec.is_readonly_finalize = self.env.user.has_group(
                 'custom_hr_recruitment_finalize.group_applicant_readonly'
             )
 
     def action_finalize_process(self):
         self.ensure_one()
         self.process_finalized = True
-        return True
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'reload',
+        }
