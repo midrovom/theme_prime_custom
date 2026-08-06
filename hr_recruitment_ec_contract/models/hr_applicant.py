5a6d82c36bcf299c69b2a3d6968628aab3e294f7
midrovom <midrovo@callphoneecuador.com>
2026-08-06 16:45:36 -0500

    nuevo

--- hr_recruitment_ec_contract/models/hr_applicant.py
+++ hr_recruitment_ec_contract/models/hr_applicant.py
@@ -75,6 +75,10 @@ class HrApplicant(models.Model):
         domain="[('empresa_id', '=', company_config_id)]"
     )
     readonly_fields = fields.Boolean(compute='_compute_readonly_fields', store=False)
+    process_finalized = fields.Boolean(
+        string="Proceso Finalizado",
+        default=False
+    )
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -241,3 +245,13 @@ class HrApplicant(models.Model):
             for rec in self:
                 rec.readonly_fields = rec.process_finalized or rec.is_readonly_group
 
+    def action_finalize_process(self):
+            self.ensure_one()
+            self.process_finalized = True
+            return {
+                'type': 'ir.actions.act_window',
+                'res_model': 'hr.applicant',
+                'view_mode': 'form',
+                'res_id': self.id,
+                'target': 'current',
+            }
