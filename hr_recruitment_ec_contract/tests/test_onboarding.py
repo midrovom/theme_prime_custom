7d1375dfc8c349332c892c4ee9eec09268c32a97
midrovom <midrovo@callphoneecuador.com>
2026-08-06 09:13:27 -0500

    nuevo

--- hr_recruitment_ec_contract/tests/test_onboarding.py
+++
@@ -1,9 +0,0 @@
-from odoo.tests import tagged
-from odoo.tests.common import TransactionCase
-
-
-@tagged("post_install", "-at_install")
-class TestEcOnboarding(TransactionCase):
-    def test_default_templates_exist(self):
-        document_types = set(self.env["hr.ec.document.template"].search([]).mapped("document_type"))
-        self.assertTrue({"contract", "thirteenth", "employee_file"}.issubset(document_types))
