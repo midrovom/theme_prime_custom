3bd7ca16a9d26db82273537e9d8caa5e50a9e0c8
midrovom <midrovo@callphoneecuador.com>
2026-08-11 14:56:50 -0500

    nuevo

--- hr_recruitment_readonly_fields/models/hr_applicant.py
+++ hr_recruitment_readonly_fields/models/hr_applicant.py
@@ -3,26 +3,30 @@ from odoo import api, fields, models
 
 _logger = logging.getLogger(__name__)
 
+
 class HrApplicant(models.Model):
     _inherit = "hr.applicant"
 
     process_finalized = fields.Boolean(string="Proceso Finalizado", default=False,)
-    is_readonly_finalize = fields.Boolean(string="Readonly después de finalizar", compute="_compute_is_readonly_finalize",
+    is_readonly_finalize = fields.Boolean(string="Usuario readonly después de finalizar", compute="_compute_is_readonly_finalize",
         store=False,
     )
 
     @api.depends_context("uid")
     def _compute_is_readonly_finalize(self):
-        has_group = self.env.user.has_group("custom_web_hr_datos_candidatos.group_applicant_readonly")
+        has_group = self.env.user.has_group(
+            "custom_web_hr_datos_candidatos.group_applicant_readonly"
+        )
 
         for rec in self:
             rec.is_readonly_finalize = has_group
 
     def action_finalize_process(self):
         self.ensure_one()
-        self.process_finalized = True
 
-        _logger.info(">>> Proceso finalizado: applicant=%s, user=%s", self.id, self.env.user.login,)
+        self.write({
+            "process_finalized": True,
+        })
 
         return {
             "type": "ir.actions.client",
