621993e7448c18ba44dbb3b7c6f012a3ce38b540
midrovom <midrovo@callphoneecuador.com>
2026-08-12 12:47:33 -0500

    nuevo

--- custom_web_candidatos/static/src/js/recruitment_documents.js
+++ custom_web_candidatos/static/src/js/recruitment_documents.js
@@ -6,81 +6,86 @@ import { _t } from "@web/core/l10n/translation";
 publicWidget.registry.MultistepForm = publicWidget.Widget.extend({
     selector: '#hr_job_recruitment_form',
 
-        events: {
-            'change #fotografia': '_onFileSelected',
-            'change #cedula-votacion': '_onFileSelected',
-            'change #historia-laboral-iess': '_onFileSelected',
-            'change #estudios-senecyt': '_onFileSelected',
-            'change #recomendaciones': '_onFileSelected',
-            'change #certificados-trabajo': '_onFileSelected',
-            'change #planilla-servicios': '_onFileSelected',
-            'change #croquis-domicilio': '_onFileSelected',
-            'change #cuenta-banco-internacional': '_onFileSelected',
-            'change #certificado-salud': '_onFileSelected',
-        },
+    events: {
 
-        //----------------------------------------------------------------------
-        // Private
-        //----------------------------------------------------------------------
+        'change #fotografia': '_onFileSelected',
+        'change #cedula-votacion': '_onFileSelected',
+        'change #historia-laboral-iess': '_onFileSelected',
+        'change #estudios-senecyt': '_onFileSelected',
+        'change #recomendaciones': '_onFileSelected',
+        'change #certificados-trabajo': '_onFileSelected',
+        'change #planilla-servicios': '_onFileSelected',
+        'change #croquis-domicilio': '_onFileSelected',
+        'change #cuenta-banco-internacional': '_onFileSelected',
+        'change #certificado-salud': '_onFileSelected',
+        'change #acta-matrimonio': '_onFileSelected',
+        'change #hijos-menores': '_onFileSelected',
+        'change #cursos-realizados': '_onFileSelected',
+        'change #formulario-107': '_onFileSelected',
+    },
 
-        _onFileSelected(ev) {
-            const input = ev.currentTarget;
-            const file = input.files[0];
-            const messageId = `#file-selected-${input.id}`;
-            if (file) {
-                this.$(messageId).text(`Archivo seleccionado: ${file.name}`);
-            } else {
-                this.$(messageId).text('');
-            }
-        },
+    //----------------------------------------------------------------------
+    // Private
+    //----------------------------------------------------------------------
 
-        _validateFile(id, validTypes) {
-            const $f = this.$(id);
-            const file = $f[0].files[0];
-            if (!file || (validTypes && !validTypes.includes(file.type))) {
-                $f.addClass('is-invalid');
-                return false;
-            }
-            $f.removeClass('is-invalid');
-            return true;
-        },
+    _onFileSelected(ev) {
+        const input = ev.currentTarget;
+        const file = input.files[0];
+        const messageId = `#file-selected-${input.id}`;
+        if (file) {
+            this.$(messageId).text(`Archivo seleccionado: ${file.name}`);
+        } else {
+            this.$(messageId).text('');
+        }
+    },
 
-        _validateDocuments() {
-            const isFotografiaValid = this._validateFile('#fotografia', ['image/jpeg','image/png']);
-            const isCedulaValid = this._validateFile('#cedula-votacion', ['application/pdf']);
-            const isHistoriaLaboralValid = this._validateFile('#historia-laboral-iess', ['application/pdf']);
-            const isEstudiosSenecytValid = this._validateFile('#estudios-senecyt', ['application/pdf']);
-            const isRecomendacionesValid = this._validateFile('#recomendaciones', ['application/pdf']);
-            const isCertificadosTrabajoValid = this._validateFile('#certificados-trabajo', ['application/pdf']);
-            const isPlanillaServiciosValid = this._validateFile('#planilla-servicios', ['application/pdf']);
-            const isCroquisDomicilioValid = this._validateFile('#croquis-domicilio', ['application/pdf']);
-            const isCuentaBancoValid = this._validateFile('#cuenta-banco-internacional', ['application/pdf']);
-            const isCertificadoSaludValid = this._validateFile('#certificado-salud', ['application/pdf']);
+    _validateFile(id, validTypes) {
+        const $f = this.$(id);
+        const file = $f[0].files[0];
+        if (!file || (validTypes && !validTypes.includes(file.type))) {
+            $f.addClass('is-invalid');
+            return false;
+        }
+        $f.removeClass('is-invalid');
+        return true;
+    },
 
-            if (
-                !isFotografiaValid ||
-                !isCedulaValid ||
-                !isHistoriaLaboralValid ||
-                !isEstudiosSenecytValid ||
-                !isRecomendacionesValid ||
-                !isCertificadosTrabajoValid ||
-                !isPlanillaServiciosValid ||
-                !isCroquisDomicilioValid ||
-                !isCuentaBancoValid ||
-                !isCertificadoSaludValid
-            ) {
-                this._scrollToFirstError();
-                return false;
-            }
-            return true;
-        },
+    _validateDocuments() {
+        const isFotografiaValid = this._validateFile('#fotografia', ['image/jpeg','image/png']);
+        const isCedulaValid = this._validateFile('#cedula-votacion', ['application/pdf']);
+        const isHistoriaLaboralValid = this._validateFile('#historia-laboral-iess', ['application/pdf']);
+        const isEstudiosSenecytValid = this._validateFile('#estudios-senecyt', ['application/pdf']);
+        const isRecomendacionesValid = this._validateFile('#recomendaciones', ['application/pdf']);
+        const isCertificadosTrabajoValid = this._validateFile('#certificados-trabajo', ['application/pdf']);
+        const isPlanillaServiciosValid = this._validateFile('#planilla-servicios', ['application/pdf']);
+        const isCroquisDomicilioValid = this._validateFile('#croquis-domicilio', ['application/pdf']);
+        const isCuentaBancoValid = this._validateFile('#cuenta-banco-internacional', ['application/pdf']);
+        const isCertificadoSaludValid = this._validateFile('#certificado-salud', ['application/pdf']);
 
-        _scrollToFirstError() {
-            const $firstError = this.$('.is-invalid').first();
-            if ($firstError.length) {
-                $('html, body').animate({
-                    scrollTop: $firstError.offset().top - 100
-                }, 500);
-            }
-        },
-    });
+        if (
+            !isFotografiaValid ||
+            !isCedulaValid ||
+            !isHistoriaLaboralValid ||
+            !isEstudiosSenecytValid ||
+            !isRecomendacionesValid ||
+            !isCertificadosTrabajoValid ||
+            !isPlanillaServiciosValid ||
+            !isCroquisDomicilioValid ||
+            !isCuentaBancoValid ||
+            !isCertificadoSaludValid
+        ) {
+            this._scrollToFirstError();
+            return false;
+        }
+        return true;
+    },
+
+    _scrollToFirstError() {
+        const $firstError = this.$('.is-invalid').first();
+        if ($firstError.length) {
+            $('html, body').animate({
+                scrollTop: $firstError.offset().top - 100
+            }, 500);
+        }
+    },
+});
