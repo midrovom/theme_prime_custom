739f28f1bd07609c0f4cdb7448f7932c92edfb75
midrovom <midrovo@callphoneecuador.com>
2026-08-12 14:43:00 -0500

    nuevo

--- custom_web_candidatos/static/src/js/recruitment_documents.js
+++ custom_web_candidatos/static/src/js/recruitment_documents.js
@@ -66,9 +66,97 @@ publicWidget.registry.MultistepForm = publicWidget.Widget.extend({
         this._renderFileList(container, input);
     },
 
+    _validateCurriculum() {
+        return this._validatePDF('#curriculum-vitae','#file-selected-curriculum-vitae','Debe adjuntar hoja de vida.');
+    },
+
+    _validateFotografia() {
+        return this._validateImageFile('#fotografia','#file-selected-fotografia','Debe adjuntar una fotografía.');
+    },
+
+    _validateCedulaVotacion() {
+        return this._validatePDF('#cedula-votacion','#file-selected-cedula-votacion','Debe adjuntar cédula y certificado de votación.');
+    },
+
+    _validateHistoriaLaboral() {
+        return this._validatePDF('#historia-laboral-iess','#file-selected-historia-laboral-iess','Debe adjuntar historia laboral.');
+    },
+
+    _validateEstudiosSenecyt() {
+        return this._validatePDF('#estudios-senecyt','#file-selected-estudios-senecyt','Debe adjuntar certificado o título.');
+    },
+
+    _validateRecomendaciones() {
+        return this._validatePDF('#recomendaciones','#file-selected-recomendaciones','Debe adjuntar recomendaciones.');
+    },
+
+    _validateCertificadosTrabajo() {
+        return this._validatePDF('#certificados-trabajo','#file-selected-certificados-trabajo','Debe adjuntar certificados de trabajo.');
+    },
+
+    _validatePlanillaServicios() {
+        return this._validatePDF('#planilla-servicios','#file-selected-planilla-servicios','Debe adjuntar planilla de servicios básicos.');
+    },
+
+    _validateCroquisDomicilio() {
+        return this._validatePDF('#croquis-domicilio','#file-selected-croquis-domicilio','Debe adjuntar croquis del domicilio.');
+    },
+
+    _validateCuentaBancoInternacional() {
+        return this._validatePDF('#cuenta-banco-internacional','#file-selected-cuenta-banco-internacional','Debe adjuntar cuenta bancaria.');
+    },
+
+    _validateCertificadoSalud() {
+        return this._validatePDF('#certificado-salud','#file-selected-certificado-salud','Debe adjuntar certificado de salud.');
+    },
+
     _refreshFileInput(input) {
         input.value = "";
     },
+
+
+    _validatePDF(inputId, containerId, message) {
+        const $input = this.$(inputId);
+        const $container = this.$(containerId);
+        const file = $input[0].files[0];
+
+        if (!file) {
+            $input.addClass('is-invalid');
+            $container.html(`<div class="text-danger fs-6">${message}</div>`);
+            return false;
+        }
+
+        if (file.type !== "application/pdf" && !file.name.toLowerCase().endsWith(".pdf")) {
+            $input.addClass('is-invalid');
+            $container.html(`<div class="text-danger fs-6">Solo se permiten archivos PDF.</div>`);
+            return false;
+        }
+
+        $input.removeClass('is-invalid');
+        return true;
+    },
+
+    _validateImageFile(inputId, containerId, message) {
+        const $input = this.$(inputId);
+        const $container = this.$(containerId);
+        const file = $input[0].files[0];
+
+        if (!file) {
+            $input.addClass('is-invalid');
+            $container.html(`<div class="text-danger fs-6">${message}</div>`);
+            return false;
+        }
+
+        if (!file.type.startsWith("image/")) {
+            $input.addClass('is-invalid');
+            $container.html(`<div class="text-danger fs-6">Solo se permiten imágenes.</div>`);
+            return false;
+        }
+
+        $input.removeClass('is-invalid');
+        return true;
+    },
+
 
     _renderFileList(container, input) {
         container.innerHTML = "";
