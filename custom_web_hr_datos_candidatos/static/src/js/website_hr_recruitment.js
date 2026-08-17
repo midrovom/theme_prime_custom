fb96ad63c9ed44982d687a7b03b0b35ceed608d8
midrovom <midrovo@callphoneecuador.com>
2026-08-17 10:08:14 -0500

    nuevo

--- custom_web_hr_datos_candidatos/static/src/js/website_hr_recruitment.js
+++ custom_web_hr_datos_candidatos/static/src/js/website_hr_recruitment.js
@@ -9,11 +9,11 @@ const optionsYears = YEARS.map(year => `<option value="${year}">${year}</option>
 let cachedCountries = null;
 let cachedStatesByCountry = {};
 
-DOCUMENT_TYPES = [
-    ('cedula', 'Cédula'),
-    ('id_extrj', 'Cédula extranjera'),
-    ('pasaporte', 'Pasaporte'),
-]
+const DOCUMENT_TYPES = [
+    ['cedula', 'Cédula'],
+    ['id_extrj', 'Cédula extranjera'],
+    ['pasaporte', 'Pasaporte'],
+];
 
 async function loadCountriesAndStates() {
     if (!cachedCountries) {
@@ -195,13 +195,10 @@ publicWidget.registry.MultistepForm = publicWidget.Widget.extend({
                         <!-- Tipo de documento -->
                         <div class="col-12 col-md-3">
                             <label for="fam-type-doc_${this.familyCount}" class="fs-6"> Tipo de documento: <span class="text-danger">*</span></label>
-                                <select id="fam-type-doc_${this.familyCount}" name="famTipoDoc_${this.familyCount}" class="form-select rounded-pill py-2" 
-                                        aria-label="Seleccionar tipo de documento" required="required">
+                                <select id="fam-type-doc_${this.familyCount}" name="famTipoDoc_${this.familyCount}" class="form-select rounded-pill py-2" aria-label="Seleccionar tipo de documento" required="required">
                                     <option selected="selected"></option>
-                                    <t t-foreach="document_types" t-as="document_type">
-                                        <option t-att-value="document_type[0]" t-esc="document_type[1]"/>
-                                    </t>
-                                </select>
+                                ${DOCUMENT_TYPES.map(doc => `<option value="${doc[0]}">${doc[1]}</option>`).join('')}
+                            </select>
                             <div class="invalid-feedback">Seleccione una opción.</div>
                         </div>
 
