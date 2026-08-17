848897ebfd2d6ece6b68ea31cfa8bd9cdaf8af5c
midrovom <midrovo@callphoneecuador.com>
2026-08-17 11:07:36 -0500

    nuevo

--- custom_web_hr_datos_candidatos/static/src/js/website_hr_recruitment.js
+++ custom_web_hr_datos_candidatos/static/src/js/website_hr_recruitment.js
@@ -1819,33 +1819,6 @@ publicWidget.registry.MultistepForm = publicWidget.Widget.extend({
         this._addFamilyBlock(parentesco); 
     },
 
-    // async _addFamilyBlock(parentesco) {
-    //     this.familyCount++;
-    //     const FAMILY_TYPES_MAP = {
-    //         '1': 'Padre',
-    //         '2': 'Madre',
-    //         '3': 'Hermano(a)',
-    //         '4': 'Conyugue',
-    //         '5': 'Hijo(a)'
-    //     };
-
-    //     const label = FAMILY_TYPES_MAP[parentesco] || parentesco;
-    //     const html = await this._getFamilyBlock(label);
-    //     this.$('#family_container').append(html);
-    //     this.$(`#family_container .family-block:last`).append(`
-    //         <input type="hidden" name="famTipo_${this.familyCount}" value="${parentesco}"/>
-    //     `);
-
-    //     const i = this.familyCount;
-    //     this.$(`input[name="famDisc_${i}"]`).on('change', () => {
-    //         this._toggleFamilyDisability(i);
-    //     });
-    //     this.$(`input[name="famDepende_${i}"]`).on('change', (ev) => {
-    //         const name = $(ev.currentTarget).attr('name');
-    //         this.$(`input[name="${name}"]`).removeClass('is-invalid');
-    //     });
-    // },
-
     async _addFamilyBlock(parentesco) {
         const FAMILY_TYPES_MAP = {
             '1': 'Padre',
@@ -1855,14 +1828,21 @@ publicWidget.registry.MultistepForm = publicWidget.Widget.extend({
             '5': 'Hijo(a)'
         };
 
-        const UNIQUE_TYPES = ['Padre', 'Madre', 'Conyugue']; // los que deben ser únicos
+        const UNIQUE_TYPES = ['Padre', 'Madre', 'Conyugue']; 
         const label = FAMILY_TYPES_MAP[parentesco] || parentesco;
 
         if (UNIQUE_TYPES.includes(label) &&
             this.$(`#family_container .family-block[data-type="${label}"]`).length > 0) {
-            alert(`Ya existe un bloque para ${label}`);
+            $('#familyMessageText').text(`Ya existe un bloque para ${label}`);
+            $('#familyMessage').removeClass('d-none');
             return;
         }
+
+        // if (UNIQUE_TYPES.includes(label) &&
+        //     this.$(`#family_container .family-block[data-type="${label}"]`).length > 0) {
+        //     alert(`Ya existe un bloque para ${label}`);
+        //     return;
+        // }
 
         this.familyCount++;
         const html = await this._getFamilyBlock(label);
