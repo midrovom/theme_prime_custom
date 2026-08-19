1e39f69b0f2f5c503d16a981bf2453ae4da18979
midrovom <midrovo@callphoneecuador.com>
2026-08-19 11:23:19 -0500

    nuevo

--- custom_web_hr_datos_candidatos/static/src/js/website_hr_recruitment.js
+++ custom_web_hr_datos_candidatos/static/src/js/website_hr_recruitment.js
@@ -359,50 +359,35 @@ publicWidget.registry.MultistepForm = publicWidget.Widget.extend({
             </div>
         `;
 
-        setTimeout(() => {
-            const i = this.familyCount;
-            const updateFullName = () => {
-                const paterno = this.$(`input[name="famApellidoPaterno_${i}"]`).val()?.trim() || "";
-                const materno = this.$(`input[name="famApellidoMaterno_${i}"]`).val()?.trim() || "";
-                const primer = this.$(`input[name="famPrimerNombre_${i}"]`).val()?.trim() || "";
-                const segundo = this.$(`input[name="famSegundoNombre_${i}"]`).val()?.trim() || "";
+        // setTimeout(() => {
+        //     const i = this.familyCount;
+        //     const updateFullName = () => {
+        //         const paterno = this.$(`input[name="famApellidoPaterno_${i}"]`).val()?.trim() || "";
+        //         const materno = this.$(`input[name="famApellidoMaterno_${i}"]`).val()?.trim() || "";
+        //         const primer = this.$(`input[name="famPrimerNombre_${i}"]`).val()?.trim() || "";
+        //         const segundo = this.$(`input[name="famSegundoNombre_${i}"]`).val()?.trim() || "";
 
-                const fullName = `${paterno} ${materno} ${primer} ${segundo}`.trim();
-                this.$(`input[name="famNombre_${i}"]`).val(fullName);
-            };
+        //         const fullName = `${paterno} ${materno} ${primer} ${segundo}`.trim();
+        //         this.$(`input[name="famNombre_${i}"]`).val(fullName);
+        //     };
 
-            this.$(`input[name="famApellidoPaterno_${i}"], 
-                    input[name="famApellidoMaterno_${i}"], 
-                    input[name="famPrimerNombre_${i}"], 
-                    input[name="famSegundoNombre_${i}"]`).on("input blur", function() {
-                const $f = $(this);
-                if (!$f.val().trim() && $f.prop("required")) {
-                    $f.addClass("is-invalid");
-                    if ($f.next(".invalid-feedback").length === 0) {
-                        $f.after('<div class="invalid-feedback">Campo obligatorio</div>');
-                    }
-                } else {
-                    $f.removeClass("is-invalid");
-                }
-                updateFullName();
-            });
+        //     this.$(`input[name="famApellidoPaterno_${i}"], 
+        //             input[name="famApellidoMaterno_${i}"], 
+        //             input[name="famPrimerNombre_${i}"], 
+        //             input[name="famSegundoNombre_${i}"]`).on("input blur", function() {
+        //         const $f = $(this);
+        //         if (!$f.val().trim() && $f.prop("required")) {
+        //             $f.addClass("is-invalid");
+        //             if ($f.next(".invalid-feedback").length === 0) {
+        //                 $f.after('<div class="invalid-feedback">Campo obligatorio</div>');
+        //             }
+        //         } else {
+        //             $f.removeClass("is-invalid");
+        //         }
+        //         updateFullName();
+        //     });
 
-            const i = this.familyCount;
-            const $tipoDoc = this.$(`#fam-type-doc_${i}`);
-            const $numDoc = this.$(`input[name="famCedula_${i}"]`);
-            const $archivoDoc = this.$(`input[name="famArchivo_${i}"]`);
-
-            $tipoDoc.on('change', () => {
-                if ($tipoDoc.val() === 'part_naci') {
-                    $numDoc.prop('disabled', true).removeAttr('required').val('').removeClass('is-invalid');
-                    $archivoDoc.prop('disabled', false).attr('required', true);
-                } else {
-                    $numDoc.prop('disabled', false).attr('required', true);
-                    $archivoDoc.prop('disabled', true).removeAttr('required').val('').removeClass('is-invalid');
-                }
-            });
-
-        }, 0);
+        // }, 0);
 
     },
 
