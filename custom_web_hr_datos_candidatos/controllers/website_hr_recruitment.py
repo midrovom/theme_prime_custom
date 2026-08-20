14c56392355bb2407f807271d15cf6cefb79ddb5
midrovom <midrovo@callphoneecuador.com>
2026-08-20 09:06:57 -0500

    nuevo

--- custom_web_hr_datos_candidatos/controllers/website_hr_recruitment.py
+++ custom_web_hr_datos_candidatos/controllers/website_hr_recruitment.py
@@ -174,7 +174,7 @@ class WebsiteHRRecruitment(http.Controller):
                         'disability_percentage': kwargs.get(f'famDiscPorcentaje_{k}'),
                         'familiar_type': tipo,
                         'filename': kwargs.get(f'famArchivo_{k}_filename'),
-                        'document_file': kwargs.get(f'famArchivo_{k}'),
+                        # 'document_file': kwargs.get(f'famArchivo_{k}'),
                     }))
 
                 k += 1
