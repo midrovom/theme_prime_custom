cfed05639129b139b60fe01ea4ec7810608c6958
midrovom <midrovo@callphoneecuador.com>
2026-09-08 15:46:42 -0500

    nuevo

--- custom_web_hr_datos_candidatos/static/src/js/website_hr_recruitment.js
+++ custom_web_hr_datos_candidatos/static/src/js/website_hr_recruitment.js
@@ -155,6 +155,11 @@ publicWidget.registry.MultistepForm = publicWidget.Widget.extend({
             this._addReferenceBlock();
         }
 
+        this._addFamilyBlock('1'); // Padre
+        this._addFamilyBlock('2'); // Madre
+        this._addFamilyBlock('4'); // Conyugue
+        this._addFamilyBlock('3'); // Hermano(a)
+
         return this._super();
     },
 
