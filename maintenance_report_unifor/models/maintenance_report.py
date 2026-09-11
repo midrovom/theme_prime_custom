87ceac96fb176ed42326b0f9d330405294da20f0
midrovom <midrovo@callphoneecuador.com>
2026-09-11 14:59:56 -0500

    nuevo

--- maintenance_report_unifor/models/maintenance_report.py
+++ maintenance_report_unifor/models/maintenance_report.py
@@ -2,8 +2,9 @@ from odoo import models, fields, _
 from odoo.exceptions import UserError
 
 class MaintenanceReportWizard(models.TransientModel):
-    _inherit = 'maintenance.report.wizard'
+    _inherit = 'maintenance.report.wizard'   # extendemos el wizard del módulo maintenance_report
 
+    # Añadimos la opción 'uniform' al campo report_type
     report_type = fields.Selection(selection_add=[
         ('uniform', 'Uniforme'),
     ])
@@ -13,6 +14,8 @@ class MaintenanceReportWizard(models.TransientModel):
 
         if not equipments:
             raise UserError(_("Debe seleccionar al menos un equipo."))
+
+        # Guardar quién entregó
         equipments.write({
             'entregado_por_id': self.entregado_por_id.id
         })
@@ -23,7 +26,8 @@ class MaintenanceReportWizard(models.TransientModel):
         elif self.report_type == 'return':
             report = self.env.ref('maintenance_report.maintenance_equipment_return_report')
         elif self.report_type == 'uniform':
-            report = self.env.ref('maintenance_report.maintenance_equipment_uniform_report')
+            # Aquí apuntamos al reporte definido en tu módulo maintenance_report_unifor
+            report = self.env.ref('maintenance_report_unifor.maintenance_equipment_uniform_report')
         else:
             raise UserError(_("Tipo de reporte no soportado."))
 
