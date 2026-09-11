ca8df90d60701dbc0bc866afd53d94c7b92047f7
midrovom <midrovo@callphoneecuador.com>
2026-09-11 15:10:25 -0500

    nuevo

--- maintenance_report_unifor/wizard/maintenance_report_wizard.py
+++ maintenance_report_unifor/wizard/maintenance_report_wizard.py
@@ -24,7 +24,7 @@ class MaintenanceReportWizard(models.TransientModel):
             report = self.env.ref('maintenance_report.maintenance_equipment_return_report')
         elif self.report_type == 'uniform':
             # Reporte definido en tu módulo personalizado
-            report = self.env.ref('maintenance_report_unifor.maintenance_equipment_uniform_report')
+            report = self.env.ref('maintenance_report.maintenance_equipment_uniform_report')
         else:
             raise UserError(_("Tipo de reporte no soportado."))
 
