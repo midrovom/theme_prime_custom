538574a757829cb4306de2938b8ced4cb1ad2a99
midrovom <midrovo@callphoneecuador.com>
2026-09-15 17:10:08 -0500

    nuevo

--- conedera_odoo_census/models/res_partner.py
+++ conedera_odoo_census/models/res_partner.py
@@ -45,7 +45,7 @@ class ResPartner(models.Model):
     census_gps_payload = fields.Char(string="Captura GPS", copy=False)
     census_gps_accuracy = fields.Float(string="Precisión GPS (m)", readonly=True, copy=False)
     census_gps_captured_at = fields.Datetime(
-        string="GPS capturado el", readonly=True, copy=False
+        string="GPS capturado el", readonly=True, copy=False, store=True
     )
 
     census_visit_ids = fields.One2many(
