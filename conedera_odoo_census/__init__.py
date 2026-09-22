c30efe4510f65a6fdf8245b9f6864fc286353723
midrovom <midrovo@callphoneecuador.com>
2026-09-22 17:06:04 -0500

    nuevo·

--- conedera_odoo_census/__init__.py
+++ conedera_odoo_census/__init__.py
@@ -1,13 +1,14 @@
-from . import census_business_type
-from . import census_mobile_brand
-from . import partner_opening_hour
-from . import res_partner
-from . import census_customer_lookup_wizard
-from . import census_unlock_request
-from . import census_reassignment_request
-from . import census_visit
-from . import sale_order
-from . import census_commercial_report
-from . import product_template
-from . import census_role_wizard
-from . import crm_team
+from . import models
+# from . import census_business_type
+# from . import census_mobile_brand
+# from . import partner_opening_hour
+# from . import res_partner
+# from . import census_customer_lookup_wizard
+# from . import census_unlock_request
+# from . import census_reassignment_request
+# from . import census_visit
+# from . import sale_order
+# from . import census_commercial_report
+# from . import product_template
+# from . import census_role_wizard
+# from . import crm_team
